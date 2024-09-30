f755ffab826dded15fe80798b239207caf021325
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-01 00:57:53 +0530

    Time: 0 ms (100%), Space: 12.3 MB (42.14%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,35 +1,28 @@
 class Solution {
-    bool binarySearch(vector<int>nums,int target){
-        int low=0;
-        int high=nums.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                return true;
-            }
-            if(nums[mid]>target){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        return false;
-    }
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        //  brute force will be the linear search 
-        //  here is the better approach than brute force
-        //  traverse every row and check if the element is in the range 
+        //  this question can be solved simply by formula 
+        // seach space will be 0 to row*col-1
+        int low=0;
+        int m=matrix.size();
         int n=matrix[0].size();
-        for(int i=0;i<matrix.size();i++){
-            if(target>=matrix[i][0] && target<=matrix[i][n-1]){
-                return binarySearch(matrix[i],target);
-            }
-        } 
+        int high=m*n-1;
 
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=(mid/n);
+            int col=mid%n;
+            if(target==matrix[row][col]) return true;
+
+            if(target>matrix[row][col]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
 
         return false;
-        // this will take O(N+logM)
     }
 };
