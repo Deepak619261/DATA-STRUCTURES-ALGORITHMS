ac977c5c3d07881e76980f00c8790f9b590d6e86
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-02 01:38:35 +0530

    Time: 121 ms (68.28%), Space: 49.3 MB (79.24%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -1,24 +1,47 @@
 class Solution {
-public:
-    vector<int> findPeakGrid(vector<vector<int>>& mat) {
-       // let's try the approach of finding the largest element 
-       vector<int>ans;
-       int row=-1;
-       int col=-1;
+    int getmaxrow(vector<vector<int>>& mat, int col, int m){
+        int ans=-1;
+        int maxi=INT_MIN;
 
-       int maxi=INT_MIN;
-
-       for(int i=0;i<mat.size();i++){
-        for(int j=0;j<mat[0].size();j++){
-            if(maxi<mat[i][j]){
-                maxi=mat[i][j];
-                row=i;
-                col=j;
+        for(int i=0;i<m;i++){
+            if(mat[i][col]>maxi){
+                maxi=mat[i][col];
+                ans=i;
             }
         }
-       }
-       ans.push_back(row);
-       ans.push_back(col);
-       return ans;
+
+        return ans;
+    }
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        int low=0;
+        int high=mat[0].size()-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            int i=getmaxrow(mat,mid,m);
+
+            int left=-1;
+            int right=-1;
+
+            int ele=mat[i][mid];
+
+            if(mid+1<n) right=mat[i][mid+1];
+            if(mid-1>=0) left=mat[i][mid-1];
+
+            if(ele>left && ele>right){
+                return {i,mid};
+            }
+            else if(left>ele){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+
+        return {-1,-1};
     }
 };
