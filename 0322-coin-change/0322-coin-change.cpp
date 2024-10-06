4d38bd2c521e3f0eb4cd0eb4e42a1d3427961525
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-06 13:12:35 +0530

    Time: 2406 ms (5.01%), Space: 664.4 MB (5.02%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    int solve(vector<int>coins, int amount ,int index,vector<vector<int>>&dp){
+        if(index==0){
+            if(amount%coins[0]==0){
+                return amount/coins[0];
+            }
+            return 1e9;
+        }
+
+
+        if(dp[index][amount]!=-1){
+            return dp[index][amount];
+        }
+
+        //  there may be two possiblities 
+        //  first we can take one coin second we can't
+        //  but when we are taking one coin then there is mentioned that supply of the coin is unlimited so we won't get to the next coin untill value of the amount is below coin  
+
+        //  taking the coin 
+        int taken=1e9;
+        if(coins[index]<=amount){
+
+            //  crux of the logic lies here in this line 
+            taken=1+solve(coins,amount-coins[index],index,dp);
+        }
+
+        int nottaken=solve(coins,amount,index-1,dp);
+
+        return dp[index][amount]=min(taken,nottaken);
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+     int n=coins.size();
+     vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+
+     int ans= solve(coins,amount,n-1,dp);
+
+     if(ans==1e9) return -1;
+
+     return ans;
+
+
+    }
+};
