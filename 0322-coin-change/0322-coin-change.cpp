43f457e21582a4e5bb5ca5a6970dc465b8193430
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-06 13:29:42 +0530

    Time: 56 ms (18.09%), Space: 47.2 MB (34.57%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -30,15 +30,41 @@ class Solution {
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
+
+     if(amount==0){
+        return 0;
+     }
      int n=coins.size();
-     vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+    //   tabulation 
+    vector<vector<int>>dp(n,vector<int>(amount+1,0));
 
-     int ans= solve(coins,amount,n-1,dp);
+    //  base condition 
+    for(int i=0;i<=amount;i++){
+        if(i%coins[0]==0){
+            dp[0][i]=i/coins[0];
+        }
+        else{
+            dp[0][i]=1e9;
+        }
+    }
 
-     if(ans==1e9) return -1;
+    for(int ind=1;ind<n;ind++){
+        for(int j=0;j<=amount;j++){
+            int taken=1e9;
+            if(coins[ind]<=j){
+                taken=1+dp[ind][j-coins[ind]];
+            }
+            int nottaken=dp[ind-1][j];
 
-     return ans;
+            dp[ind][j]=min(taken,nottaken);
+        }
+    }
 
+    int ans=dp[n-1][amount];
+    
+    if(ans>=1e9) return -1;
+
+    return ans;
 
     }
 };
