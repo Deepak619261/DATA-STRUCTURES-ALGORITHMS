a6de4fc69be30e256e292d578b18fb6c2af9b3c8
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-09 02:44:43 +0530

    Time: 15 ms (45.23%), Space: 44 MB (7.05%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -4,29 +4,31 @@ public:
     int change(int amount, vector<int>& coins) {
         
         int n=coins.size();
-        vector<vector<double>>dp(n,vector<double >(amount+1,0));
+        vector<unsigned long int >prev(amount+1,0);
 
         //  tabulation 
         for(int i=0;i<=amount;i++){
         if(i%coins[0]==0){
-            dp[0][i]=1;
+            prev[i]=1;
         }
         }
         
         
     for(int ind=1;ind<n;ind++){
+        vector<unsigned long int>curr(amount+1,0);
         for(int j=0;j<=amount;j++){
-            double taken=0;
+            unsigned long int  taken=0;
             if(coins[ind]<=j){
-                taken=dp[ind][j-coins[ind]];
+                taken=curr[j-coins[ind]];
             }
-            double nottaken=dp[ind-1][j];
-            dp[ind][j]=(taken+nottaken);
+            unsigned long int nottaken=prev[j];
+            curr[j]=(taken+nottaken);
         }
+        prev=curr;
     }
 
 
-    return dp[n-1][amount];
+    return prev[amount];
     // return 0;
 
 
