a1f8e152817f7f027589171226d4f83c575f55b2
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-09 02:37:42 +0530

    Time: 33 ms (13.11%), Space: 44.8 MB (6.12%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,40 +1,34 @@
 class Solution {
-        int solve(vector<int>coins, int amount ,int index,vector<vector<int>>&dp){
-        if(index==0){
-            if(amount%coins[0]==0){
-                return 1;
-            }
-            return 0;
-        }
 
-
-        if(dp[index][amount]!=-1){
-            return dp[index][amount];
-        }
-
-        //  there may be two possiblities 
-        //  first we can take one coin second we can't
-        //  but when we are taking one coin then there is mentioned that supply of the coin is unlimited so we won't get to the next coin untill value of the amount is below coin  
-
-        //  taking the coin 
-        int taken=0;
-        if(coins[index]<=amount){
-
-            //  crux of the logic lies here in this line 
-            taken=solve(coins,amount-coins[index],index,dp);
-        }
-
-        int nottaken=solve(coins,amount,index-1,dp);
-
-        return dp[index][amount]=taken+nottaken;
-    }
 public:
     int change(int amount, vector<int>& coins) {
         
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        vector<vector<double>>dp(n,vector<double >(amount+1,0));
 
-        return solve(coins,amount,n-1,dp);
+        //  tabulation 
+        for(int i=0;i<=amount;i++){
+        if(i%coins[0]==0){
+            dp[0][i]=1;
+        }
+        }
+        
+        
+    for(int ind=1;ind<n;ind++){
+        for(int j=0;j<=amount;j++){
+            double taken=0;
+            if(coins[ind]<=j){
+                taken=dp[ind][j-coins[ind]];
+            }
+            double nottaken=dp[ind-1][j];
+            dp[ind][j]=(taken+nottaken);
+        }
+    }
+
+
+    return dp[n-1][amount];
+    // return 0;
+
 
     }
 };
