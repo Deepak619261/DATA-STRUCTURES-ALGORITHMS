6e986bc21f1aef78f47f0453c6ba2f1a762758aa
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-10 16:33:24 +0530

    Time: 12 ms (13.27%), Space: 17.1 MB (31.21%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,76 +1,77 @@
 class Solution {
-    int solve(vector<int>nums,int index,int buy,vector<vector<int>>&dp){
-        if(index==nums.size()){
-            return 0;
-        }
+    // int solve(vector<int>nums,int index,int buy,vector<vector<int>>&dp){
+    //     if(index==nums.size()){
+    //         return 0;
+    //     }
 
-        if(dp[index][buy]!=-1){
-            return dp[index][buy];
-        }
+    //     if(dp[index][buy]!=-1){
+    //         return dp[index][buy];
+    //     }
 
-        //  buy==0 means we can either buy the stock or do nothing 
-        int takeStock=0;
-        int nottakStock=0;
-        if(buy==0){
+    //     //  buy==0 means we can either buy the stock or do nothing 
+    //     int takeStock=0;
+    //     int nottakStock=0;
+    //     if(buy==0){
             
-            takeStock=-nums[index]+solve(nums,index+1,1-buy,dp);
-            nottakStock=solve(nums,index+1,buy,dp);
+    //         takeStock=-nums[index]+solve(nums,index+1,1-buy,dp);
+    //         nottakStock=solve(nums,index+1,buy,dp);
     
-        }
+    //     }
 
-        int bought=max(takeStock,nottakStock);
+    //     int bought=max(takeStock,nottakStock);
 
-        int sellStock=0;
-        int notsellStock=0;
+    //     int sellStock=0;
+    //     int notsellStock=0;
 
-        if(buy==1){
+    //     if(buy==1){
 
-        sellStock=nums[index]+solve(nums,index+1,1-buy,dp);
-        notsellStock=solve(nums,index+1,buy,dp);
-        }
+    //     sellStock=nums[index]+solve(nums,index+1,1-buy,dp);
+    //     notsellStock=solve(nums,index+1,buy,dp);
+    //     }
 
-        int sold=max(sellStock,notsellStock);
+    //     int sold=max(sellStock,notsellStock);
 
 
-        return dp[index][buy]= max(sold,bought);
+    //     return dp[index][buy]= max(sold,bought);
 
-    }
+    // }
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
 
         //  whenever we see the pattern of multiple ways and something like pick and not pick
         //  -> recursion -> try all ways -> DP
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        vector<int>prev(2,0);
         //  doing tabulations
-        dp[n][1]=0;
-        dp[n][0]=0;
+        
 
         for(int i=n-1;i>=0;i--){
+            vector<int>curr(2,0);
             for(int j=0;j<=1;j++){
                 int pickstock=0;
                 int notpickstock=0;
 
                 if(j==0){
-                    pickstock=(-prices[i])+dp[i+1][1-j];
-                    notpickstock=dp[i+1][j];
+                    pickstock=(-prices[i])+prev[1-j];
+                    notpickstock=prev[j];
                 }
                 int bought=max(pickstock,notpickstock);
 
                 int dropstock=0;
                 int notdropstock=0;
                 if(j==1){
-                    dropstock=prices[i]+dp[i+1][1-j];
-                    notdropstock=dp[i+1][j];
+                    dropstock=prices[i]+prev[1-j];
+                    notdropstock=prev[j];
                 }
 
                 int sold=max(dropstock,notdropstock);
 
-                dp[i][j]=max(sold,bought);
+                curr[j]=max(sold,bought);
             }
+            prev=curr;
         }
 
-        return dp[0][0];
+        return prev[0];
 
         
     }
