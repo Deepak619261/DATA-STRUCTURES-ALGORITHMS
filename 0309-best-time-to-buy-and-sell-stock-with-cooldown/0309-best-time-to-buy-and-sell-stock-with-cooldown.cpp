ee723bf9f8b0e703ddea97b232f8571a12c1aae0
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-11 19:56:33 +0530

    Time: 4 ms (55.61%), Space: 14.1 MB (74.27%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,52 +1,38 @@
 class Solution {
-    long solve(vector<int>nums,int index,int buy,vector<vector<long>>&dp){
-        if(index>=nums.size()){
-            return 0;
-        }
 
-        if(dp[index][buy]!=-1){
-            return dp[index][buy];
-        }
-
-        long profit=-1e9;
-        if(buy==0){
-            profit=max((long)(-nums[index]+solve(nums,index+1,!buy,dp)),solve(nums,index+1,buy,dp));
-        }
-        else{
-            profit=max((long)(nums[index]+solve(nums,index+2,!buy,dp)),solve(nums,index+1,buy,dp));
-        }
-
-        return dp[index][buy]=profit;
-    }
 public:
     int maxProfit(vector<int>& prices) {
 
         int n=prices.size();
-        vector<vector<long>>dp(n+1,vector<long>(2,0));
+        vector<long>front1(2,0);
+        vector<long>front2(2,0);
         //  lets do tabulation
 
 
         for(int i=n-1;i>=0;i--){
+            vector<long>curr(2,0);
             for(int j=0;j<2;j++){
                 long profit=0;
                 if(j==0){
-                    profit=max((long)(-prices[i]+dp[i+1][!j]),dp[i+1][j]);
+                    profit=max((long)(-prices[i]+front1[!j]),front1[j]);
                 }
                 else{
                     if(i+2<n){
-                        profit=max((long)(prices[i]+dp[i+2][!j]),dp[i+1][j]);
+                        profit=max((long)(prices[i]+front2[!j]),front1[j]);
                     }
                     else{
                         //  but you can still sell the stock on this day 
-                        profit=max((long)prices[i],dp[i+1][j]);  // we are taking maximum of selling on that day and previous profit if we don't 
+                        profit=max((long)prices[i],front1[j]);  // we are taking maximum of selling on that day and previous profit if we don't 
                     }
                 }
-                dp[i][j]=profit;
+                curr[j]=profit;
             }
+            front2=front1;
+            front1=curr;
         }
 
         
-        return (int)dp[0][0];
+        return (int)front1[0];
         
     }
 };
