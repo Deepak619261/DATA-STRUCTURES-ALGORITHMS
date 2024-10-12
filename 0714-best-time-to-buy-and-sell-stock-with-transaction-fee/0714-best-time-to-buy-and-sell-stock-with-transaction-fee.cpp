43d944e5519d13b7745ba0560fd5b7a540fcb901
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-13 02:33:58 +0530

    Time: 161 ms (31.68%), Space: 80 MB (61.24%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,54 +1,57 @@
 class Solution {
-    long solve(vector<int>nums, int fee, int index,int buy,vector<vector<long>>&dp){
-        if(index==nums.size()){
-            return 0;
-        }
+    // long solve(vector<int>nums, int fee, int index,int buy,vector<vector<long>>&dp){
+    //     if(index==nums.size()){
+    //         return 0;
+    //     }
 
-        if(dp[index][buy]!=-1){
-            return dp[index][buy];
-        }
+    //     if(dp[index][buy]!=-1){
+    //         return dp[index][buy];
+    //     }
 
 
-        long profit=0;
+    //     long profit=0;
 
-        if(buy==0){
-            profit=max((long)(-nums[index]+solve(nums,fee,index+1,!buy,dp)),solve(nums,fee,index+1,buy,dp));
-        }
-        else{
-            profit=max((long)(nums[index]-fee+solve(nums,fee,index+1,!buy,dp)),solve(nums,fee,index+1,buy,dp));
-        }
+    //     if(buy==0){
+    //         profit=max((long)(-nums[index]+solve(nums,fee,index+1,!buy,dp)),solve(nums,fee,index+1,buy,dp));
+    //     }
+    //     else{
+    //         profit=max((long)(nums[index]-fee+solve(nums,fee,index+1,!buy,dp)),solve(nums,fee,index+1,buy,dp));
+    //     }
 
-        return dp[index][buy]=profit;
-    }
+    //     return dp[index][buy]=profit;
+    // }
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n=prices.size();
-        vector<vector<long>>dp(n+1,vector<long>(2,0)); 
+        vector<int>prev(2,0);
 
-        // let's do tabulation 
+        // let's space optimize it 
 
         for(int i=n-1;i>=0;i--){
+            vector<int>curr(2,0);
             for(int j=0;j<2;j++){
                 long profit=0;
                 if(j==0){
                     
-                    profit=max(-prices[i]+dp[i+1][!j],dp[i+1][j]);
+                    profit=max(-prices[i]+prev[!j],prev[j]);
 
                 }
                 else{
 
-                    profit=max(prices[i]-fee+dp[i+1][!j],dp[i+1][j]);
+                    profit=max(prices[i]-fee+prev[!j],prev[j]);
 
                 }
-                dp[i][j]=profit;
+                curr[j]=profit;
             }
+
+            prev=curr;
 
         }
 
 
 
          
-        return dp[0][0];
+        return prev[0];
         
     }
 };
