0b7f51a71ce76181ae9e964d3201af1a94e2c227
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-10-19 17:03:48 +0530

    Time: 4 ms (91.45%), Space: 11.2 MB (44.5%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -6,28 +6,14 @@ public:
 
         //  precompute the maximum element for each subarray 
 
-        vector<vector<int>>maxi(n,vector<int>(n,INT_MIN));
-
-        for(int i=0;i<n;i++){
-            maxi[i][i]=arr[i];
-        }
-        
-
-        for(int len=2;len<=n;len++){
-            for(int i=0;i<=n-len;i++){
-                int j=i+len-1;
-               
-                maxi[i][j]=max(maxi[i][j-1],arr[j]);
-            }
-        }
-
 
         for(int i=n-1;i>=0;i--){
             
         int lim=min(i+k,n);
         int ans=INT_MIN;
+        int maxele=INT_MIN;
         for(int j=i;j<lim;j++){
-            int maxele=maxi[i][j];
+            maxele=max(maxele,arr[j]);
             int sum=(j-i+1)*maxele+dp[j+1];
             ans=max(sum,ans);
         }
