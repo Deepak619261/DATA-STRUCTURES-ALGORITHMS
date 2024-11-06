1c660bccfe446cbc76607e67a50c94be579a456a
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-11-06 23:08:03 +0530

    Time: 3 ms (70.73%), Space: 12.1 MB (83.96%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+   void solve(int open,int close, vector<string>&result,string &str){
+    if(open==0 && close==0){
+        result.push_back(str);
+        return;
+    }
+
+    if(close==open){
+        str+='(';
+        solve(open-1,close,result,str);
+        str.pop_back();
+    }
+    else if(open==0){
+        str+=')';
+        solve(open,close-1,result,str);
+        str.pop_back();
+    }
+    else if(close==0){
+        str+='(';
+        solve(open-1,close,result,str);
+        str.pop_back();
+    }
+    else{
+        str+='(';
+        solve(open-1,close,result,str);
+        str.pop_back();
+
+        str+=')';
+        solve(open,close-1,result,str);
+        str.pop_back();
+    }
+   }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        int opencnt=n;
+        int closecnt=n;
+        string str="";
+        solve(opencnt,closecnt,ans,str);
+        return ans;
+
+    }
+};
