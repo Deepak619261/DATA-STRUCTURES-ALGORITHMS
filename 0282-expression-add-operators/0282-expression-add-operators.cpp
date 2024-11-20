f0d59276f77f669cf690cf7af02d80bd0d20288b
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2024-11-21 02:40:50 +0530

    Time: 506 ms (23.96%), Space: 92.2 MB (23.62%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,55 +1,36 @@
 class Solution {
-    private:
-    void solve(string num,int target ,string sumpath,long sum, long prev,int index, vector<string>&ans){
+
+    void solve(string num, int target , vector<string>&ans,string store,long long sum , long long prev,int index){
         if(index==num.size()){
             if(sum==target){
-                ans.push_back(sumpath);
+                ans.push_back(store);
             }
-            return;
+
+            return ;
         }
 
-
-        // for every index we will go for every possiblity it may be start from 1 /2/3
         for(int i=index;i<num.size();i++){
-            // there was leading zero case but i'm going without considering that case ..i'll later 
-             if(i>index && num[index]=='0'){
-                 break;
-                //   avoid leading zeros here ..
-             }
-            // come to realise what it was 
+        
+            if (i > index && num[index] == '0') break;
 
+            long long  number=stoll(num.substr(index,i-index+1));
+            string temppath=num.substr(index,i-index+1);
 
-                long number=stol(num.substr(index,i-index+1));
-                // simaltaneously we will create string to store the path
-                string temppath=num.substr(index,i-index+1);
             if(index==0){
-                
-                solve(num,target,temppath,number,number,i+1,ans);
+                 solve(num,target,ans,temppath,number,number,i+1);
             }
             else{
-                // go for every possiblity possible 
-                // the very first we can go for the plus opreation 
-                solve(num,target,sumpath+'+'+temppath,sum+number,number,i+1,ans);
-
-                // then we can go the minus opreation 
-                solve(num, target,sumpath+'-'+temppath,sum-number,-number,i+1,ans);
-            
-                //  then we have to go to the case of multiplication but for that we have undo the previosus
-                // opreation before doing further calculation 
-                solve(num,target,sumpath+'*'+temppath,sum-prev+(prev*number),prev*number,i+1,ans);
-
+            solve(num,target,ans,store+'+'+temppath,sum+number,number,i+1);
+            solve(num,target,ans,store+'-'+temppath,sum-number,-number,i+1);
+            solve(num,target,ans,store+'*'+temppath,(sum-prev)+prev*number,prev*number,i+1);
             }
+           
         }
     }
 public:
     vector<string> addOperators(string num, int target) {
-        // approach by watching the solution section ...
-        // in this approach what we are doing is that we are ...creating a string simaltaneously and 
-        // storing the result and passing it to the next function ...here is different two cases in case
-        //  of when we are just taking the very first 
         vector<string>ans;
-        solve(num,target,"",0,0,0,ans);
+        solve(num,target,ans,"",0,0,0);
         return ans;
-        
     }
 };
