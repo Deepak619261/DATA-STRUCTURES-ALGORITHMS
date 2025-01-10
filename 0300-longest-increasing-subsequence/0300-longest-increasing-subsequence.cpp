f3b736c471ee13eccf982a5e1b010edfc4d71b64
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-01-11 00:29:23 +0530

    Time: 483 ms (22.54%), Space: 295 MB (27.15%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,45 @@
 class Solution {
-    int solve(int index, int previndex, vector<int>&nums,vector<vector<int>>&dp){
-        if(index==nums.size())return 0;
-
-        if(dp[index][previndex+1]!=-1)return dp[index][previndex+1];
-
-        int notpick=solve(index+1,previndex,nums,dp);
-        int pick=0;
-
-        if(previndex==-1 || nums[index]>nums[previndex]){
-           pick=1+solve(index+1,index,nums,dp);
+    int solve(vector<int>nums, int index, int prev_index,vector<vector<int>>&dp){
+        if(index==nums.size()){
+            return 0;
+        }
+        
+        if(dp[index][prev_index+1]!=-1){
+            return dp[index][prev_index+1];
         }
 
-        return dp[index][previndex+1]=max(pick,notpick);
+        //  there will be two cases eitheir pick or not pick 
+        int notpick=solve(nums,index+1, prev_index,dp);
+
+        int pick=0;
+
+        //  don't pick
+        if(prev_index==-1 || nums[index]>nums[prev_index]){
+           pick=1+solve(nums,index+1,index,dp); 
+        }
+
+
+        return dp[index][prev_index+1]=max(pick,notpick);
     }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        //  ah , i forgot how to tabulate the recursive solution 
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(0,-1,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        //  let's tabulate the solution 
 
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=-1;j--){
+                int not_pick=dp[i+1][j+1];
+                int pick=0;
+                if(j==-1 || nums[i]>nums[j]){
+                    pick=1+dp[i+1][i+1];
+                }
+
+                dp[i][j+1]=max(pick,not_pick);
+            }
+        }
+
+        
+        return dp[0][0];
     }
 };
