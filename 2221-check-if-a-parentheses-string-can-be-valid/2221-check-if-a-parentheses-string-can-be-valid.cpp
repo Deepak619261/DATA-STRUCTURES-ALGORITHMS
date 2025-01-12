92e08a0730661841413b595dde41209e7ce0a86b
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-01-12 19:34:27 +0530

    Time: 7 ms (95.69%), Space: 30.2 MB (53.05%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,66 +1,55 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        int n=s.size();
+        // intution of the problem 
+        //  brute force that we can try every combination by recursion that will take 2^n and given 10^5 constraints it can be only solved in O(n) time complexity 
+        //  next we can use stack for the order and parenthesis thing but handling locked and unlocked cases will add the complexity Using a Stack Utilize a stack to track opening parentheses and ensure proper matching.
+        /*
+Adaptation for Locked Characters:
+Locked '(' or ')': Handle normally.
+Unlocked Characters: Treat them as either '(' or ')', introducing flexibility.
+Challenges:
 
-        // the last bracked can't be remain as open and locked 
-        if((locked[n-1]=='1' && s[n-1]=='(') || s.size()%2!=0)return false;
+Deciding when to treat an unlocked character as '(' or ')' dynamically is complex.
+Managing multiple possibilities increases complexity, potentially leading to inefficiency.*/ 
 
-        
-        int fixedop=0;
-        int varcnt=0;
 
-       
-        
+// the effiecient appraoch remains using counters so what we can do that we can check for every locked ')' if these can be balanced and the same thing can be done from the back , traversing in the reverse direction , which was the last submission 
 
-       for(int i=0;i<s.size();i++){
-          if(s[i]==')' && locked[i]=='1'){
-             if(fixedop==0 && varcnt==0)return false;
-             else if(varcnt!=0){
-                varcnt--;
-             }
-             else if(fixedop!=0){
-                fixedop--;
-             }
-          }
-          else if(locked[i]=='1'){
-                fixedop++;
-          }
-          else{
-            varcnt++;
-          }
-       }
-       
 
-       fixedop=0;
-       varcnt=0;
+// now comes the efficient aprraoch 
 
-  for(int i=s.size()-1;i>=0;i--){
-          if(s[i]=='(' && locked[i]=='1'){
-             if(fixedop==0 && varcnt==0)return false;
-             else if(varcnt!=0){
-                varcnt--;
-             }
-             else if(fixedop!=0){
-                fixedop--;
-             }
-          }
-          else if(locked[i]=='1'){
-                fixedop++;
-          }
-          else{
-            varcnt++;
-          }
-       }
-       
-    
+// invalid case 
+if(s.size()%2!=0)return false;
+
+if(s.back()=='(' && locked.back()=='1' || s[0]==')' && locked[0]=='1')return false;
+
+int maxop=0;
+int minop =0;
+
+for(int i=0;i<s.size();i++){
+    if(locked[i]=='1'){
+        if(s[i]=='('){
+            maxop++;
+            minop++;
+        }
+        else{
+            maxop--;
+            minop--;
+        }
+    }
+    else{
+        minop--; // considering the unlocked character can be ')' for seeing that if there remains a open bracket at the end of the traverrsal 
+        maxop++;
+    }
+
+    minop=max(0,minop);
+
+    if(maxop<0)return false;
+}
 
 
 
-
-
-    return true;
-        
-
+return minop==0;
     }
 };
