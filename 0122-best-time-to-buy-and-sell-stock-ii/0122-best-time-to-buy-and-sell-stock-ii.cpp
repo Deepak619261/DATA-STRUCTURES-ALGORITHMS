057466b38005fdea1551cc8094f1e72387e60e3d
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-01-30 12:06:05 +0530

    Time: 11 ms (5.88%), Space: 19.6 MB (26.53%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,68 +1,119 @@
 class Solution {
-    // int solve(vector<int>nums,int index,int buy,vector<vector<int>>&dp){
-    //     if(index==nums.size()){
-    //         return 0;
-    //     }
+    int solve(vector<int>nums,int index,int buy,vector<vector<int>>&dp){
+        if(index==nums.size()){
+            return 0;
+        }
 
-    //     if(dp[index][buy]!=-1){
-    //         return dp[index][buy];
-    //     }
+        if(dp[index][buy]!=-1){
+            return dp[index][buy];
+        }
 
-    //     //  buy==0 means we can either buy the stock or do nothing 
-    //     int takeStock=0;
-    //     int nottakStock=0;
-    //     if(buy==0){
+        //  buy==0 means we can either buy the stock or do nothing 
+        int takeStock=0;
+        int nottakStock=0;
+        if(buy==0){
             
-    //         takeStock=-nums[index]+solve(nums,index+1,1-buy,dp);
-    //         nottakStock=solve(nums,index+1,buy,dp);
+            takeStock=-nums[index]+solve(nums,index+1,1-buy,dp);
+            nottakStock=solve(nums,index+1,buy,dp);
     
-    //     }
+        }
 
-    //     int bought=max(takeStock,nottakStock);
+        int bought=max(takeStock,nottakStock);
 
-    //     int sellStock=0;
-    //     int notsellStock=0;
+        int sellStock=0;
+        int notsellStock=0;
 
-    //     if(buy==1){
+        if(buy==1){
 
-    //     sellStock=nums[index]+solve(nums,index+1,1-buy,dp);
-    //     notsellStock=solve(nums,index+1,buy,dp);
-    //     }
+        sellStock=nums[index]+solve(nums,index+1,1-buy,dp);
+        notsellStock=solve(nums,index+1,buy,dp);
+        }
 
-    //     int sold=max(sellStock,notsellStock);
+        int sold=max(sellStock,notsellStock);
 
 
-    //     return dp[index][buy]= max(sold,bought);
+        return dp[index][buy]= max(sold,bought);
 
-    // }
+    }
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
 
-        //  whenever we see the pattern of multiple ways and something like pick and not pick
-        //  -> recursion -> try all ways -> DP
-        vector<int>prev(2,0);
-        //  doing tabulations
+
+       vector<vector<int>>dp(n+1,vector<int>(2,0));
+    //    return solve(prices,0,0,dp);
+       
+       for(int index=n-1;index>=0;index--){
         
+        for(int buy=0;buy<2;buy++){
 
-        for(int i=n-1;i>=0;i--){
-            vector<int>curr(2,0);
-            for(int j=0;j<=1;j++){
-                long profit=0;
-
-                if(j==0){
-                    profit=max((long)(-prices[i]+prev[1-j]),(long)prev[j]);
-                }
-                else{
-                    profit=max((long)(prices[i]+prev[1-j]),(long)prev[j]);
-                }
-
-                curr[j]=profit;
-            }
-            prev=curr;
+        
+        int takeStock=0;
+        int nottakStock=0;
+        if(buy==0){
+            
+            takeStock=-prices[index]+dp[index+1][1-buy];
+            nottakStock=dp[index+1][buy];
+    
         }
 
-        return prev[0];
+        int bought=max(takeStock,nottakStock);
+
+        int sellStock=0;
+        int notsellStock=0;
+
+        if(buy==1){
+
+        sellStock=prices[index]+dp[index+1][1-buy];
+        notsellStock=dp[index+1][buy];
+        }
+
+        int sold=max(sellStock,notsellStock);
+
+
+        dp[index][buy]= max(sold,bought);
+        }
+       }
+        
+       return dp[0][0];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // //  whenever we see the pattern of multiple ways and something like pick and not pick
+        // //  -> recursion -> try all ways -> DP
+        // vector<int>prev(2,0);
+        // //  doing tabulations
+        
+
+        // for(int i=n-1;i>=0;i--){
+        //     vector<int>curr(2,0);
+        //     for(int j=0;j<=1;j++){
+        //         long profit=0;
+
+        //         if(j==0){
+        //             profit=max((long)(-prices[i]+prev[1-j]),(long)prev[j]);
+        //         }
+        //         else{
+        //             profit=max((long)(prices[i]+prev[1-j]),(long)prev[j]);
+        //         }
+
+        //         curr[j]=profit;
+        //     }
+        //     prev=curr;
+        // }
+
+        // return prev[0];
 
         
     }
