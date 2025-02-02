319d6b96046386074a6d80e1f462fb94a502a9dd
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-02-02 15:29:10 +0530

    Time: 0 ms (100%), Space: 11 MB (98.99%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        if(nums.size()<=2)return true;
 
-        int cnt=0;
-
-        if(nums[0]<nums.back())cnt++;
-
+        if(nums.size()==1)return true;
+        
+        int count=0;
 
         for(int i=1;i<nums.size();i++){
-            if(nums[i-1]>nums[i])cnt++;
+            if(nums[i]<nums[i-1])count++;
         }
 
+        if(nums[0]<nums.back())count++;
 
-        return cnt<=1;
+        // for a array to be sorted in non decreasing pair , there can exist at most one inversion pair , because suppose we got first inversion pair in middle that means that is the starting point of sorted array and think in cyclic manner then there should not exist any inversion pair in the array and the first element shoudl be greater than the last as we have already found one inversion pair and if by chance we get more than one inversion pair than we can sort the array by rotating it , there can be zero inversion pair if there are same elements 
+        return count<=1;        
+
+
     }
 };
