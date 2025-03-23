5991bfb13c722496efba400dd49f682bbd1d6a25
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-03-23 17:33:51 +0530

    Time: 603 ms (5%), Space: 338.9 MB (5.1%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,36 +1,37 @@
 class Solution {
+    void dfs(int node, vector<vector<int>>adj,vector<int>&visited,int &e, int &n){
+        visited[node]=1;
+        n++;
+        e+=adj[node].size();
+        for(auto it:adj[node]){
+            if(!visited[it]){
+                dfs(it,adj,visited,e,n);
+            }
+        }
+    }
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        // the whole bottleneck was to knowing that in a complete graph -> the number of edges are n*(n-1)/2 and 
+        // and for every component we just needed to check these things 
         vector<vector<int>>adj(n);
-
-        for(int i=0;i<n;i++){
-            adj[i].push_back(i);
+        
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
 
-       // create adjacency list 
-       for(auto it:edges){
-        adj[it[0]].push_back(it[1]);
-        adj[it[1]].push_back(it[0]);
-       }
 
-    //    unordered_map<vector<int>,int>mpp;
-       map<vector<int>,int>mpp;
+        vector<int>visited(n,0);
+        int ans=0;
 
-       for(int i=0;i<n;i++){
-          vector<int>temp=adj[i];
-          sort(temp.begin(),temp.end());
-          mpp[temp]++;
-       }
-
-       int count=0;
-
-       for(auto it:mpp){
-          if(it.first.size()==it.second){
-            count++;
-          }
-       }
-
-       return count;
-
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                int edgecount=0;
+                int nodescount=0;
+                dfs(i,adj,visited,edgecount,nodescount);
+                if(edgecount==(nodescount*(nodescount-1)))ans++;
+            }
+        }
+        return ans;
     }
 };
