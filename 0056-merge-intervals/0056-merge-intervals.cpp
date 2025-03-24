70c7571b8f52c41dfe631d2a0d1b54227b7a348b
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-03-24 21:51:47 +0530

    Time: 10 ms (18.6%), Space: 24.3 MB (26.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,46 +1,26 @@
 class Solution {
-    static bool comp(vector<int>v1, vector<int>v2){
-        if(v1[1]<v2[1]) return true;
-        if(v1[1]>v2[1]) return false;
-
-        if(v1[0]<v2[0]) return true;
-
-        return false;
-    }
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        // return intervals;
-        
+        sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
-
-        ans.push_back(intervals[0]);
-
-        int lim=ans.back()[1];
-
-        //  if the starting of the given interval is greater than the limit than for sure its a seprate interval 
-        //  if its less than there may be two possiblity that the starting index of the current index is <= than the previous intervals starting index
+        int left=intervals[0][0];
+        int rightlim=intervals[0][1];
 
         for(int i=1;i<intervals.size();i++){
-
-            if(lim<intervals[i][0]){
-                ans.push_back(intervals[i]);
+            if(intervals[i][0]>rightlim){
+                vector<int>temp={left,rightlim};
+                ans.push_back(temp);
+                left=intervals[i][0];
+                rightlim=intervals[i][1];
             }
             else{
-                // if(intervals[i][0]!=intervals[i][1]){
-               ans.back()[0]=min(ans.back()[0],intervals[i][0]);
-               ans.back()[1]=max(ans.back()[1],intervals[i][1]);
-             
-                // }
-                // else{
-                //     ans.push_back(intervals[i]);
-                // }
+                rightlim=max(rightlim,intervals[i][1]);
             }
-              lim=ans.back()[1];
-
         }
+        
+        vector<int>temp={left,rightlim};
+        ans.push_back(temp);
 
         return ans;
-
     }
 };
