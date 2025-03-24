03a1f628b7676ea16f2830fc292b6e32554bfc9b
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-03-25 00:02:52 +0530

    Time: 161 ms (6.85%), Space: 325.4 MB (21.56%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        //  a very efficient algortihm -> sweep line algorithm 
-        vector<int>arr(nums.size()+1,0);
+        int n=nums.size();
+        vector<int>temp(n+1,0);
 
-        for(int i=0;i<queries.size();i++){
-            arr[queries[i][0]]++;
-            arr[queries[i][1]+1]--;
+        for(auto it:queries){
+            temp[it[0]]++;
+            temp[it[1]+1]--;
         }
-        
 
-        int val=0;
+        int op=0;
         for(int i=0;i<nums.size();i++){
-            val=val+arr[i];
-
-            //  we are comparing value all the values accumulated , if its not greator than given value then it can't make it zero never 
-            if(val<nums[i]) return false;
+            op+=temp[i];
+            if(op<nums[i])return false;
         }
-
         return true;
     }
 };
