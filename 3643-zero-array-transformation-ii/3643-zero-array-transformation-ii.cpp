6754480070eef9b80fbb389114fd7759bfe92e97
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-03-26 00:18:23 +0530

    Time: 77 ms (15.37%), Space: 347.5 MB (14.1%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -1,45 +1,41 @@
 class Solution {
+    bool isAns(int mid,vector<int>&nums,vector<vector<int>>&queries){
+        vector<int>diff(nums.size()+1,0);
 
-    bool isvalid(int k,vector<vector<int>>& queries,vector<int>nums){
-        vector<int>arr(nums.size()+1);
-
-        for(int i=0;i<k;i++){
-            arr[queries[i][0]]+=queries[i][2];
-            arr[queries[i][1]+1]-=queries[i][2];
+        for(int i=0;i<mid;i++){
+            diff[queries[i][0]]+=queries[i][2];
+            diff[queries[i][1]+1]-=queries[i][2];
         }
 
         int op=0;
-        for(int i=0;i<arr.size()-1;i++){
-            op=op+arr[i];
-            arr[i]=op;
-            if(arr[i]<nums[i])return false;
+
+        for(int i=0;i<nums.size();i++){
+            op+=diff[i];
+            if(op<nums[i])return false;
         }
 
         return true;
-
     }
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        //  going with approach given in soutions 
-        int low=0;
-        int high=queries.size()+1;
-        int ans=-1;
-
-        while(low<high){
-             int mid=low+(high-low)/2;
-             if(isvalid(mid,queries,nums)){
-                ans=mid;
-                high=mid;
-             }
-             else{
-                low=mid+1;
-             }
+        int s=0;
+        int e=queries.size();
+        int ans=INT_MAX;
+        
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(isAns(mid,nums,queries)){
+                ans=min(ans,mid);
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
         }
 
-        if(ans<=queries.size()){
-            return ans;
-        }
+        if(ans==INT_MAX)return -1;
 
-        return -1;
+        return ans;
+
     }
 };
