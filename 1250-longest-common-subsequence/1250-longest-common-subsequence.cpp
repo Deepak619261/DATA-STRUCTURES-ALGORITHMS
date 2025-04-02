54a63a64d2d046b577a54c68040600b123c37b06
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-04-03 00:35:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,24 +1,34 @@
 class Solution {
- 
+    int solve(int index1, int index2, string text1, string text2,vector<vector<int>>&dp){
+        if(index1<0 || index2<0)return 0;
+
+        if(dp[index1][index2]!=-1)return dp[index1][index2];
+
+        if(text1[index1]==text2[index2])return 1+solve(index1-1,index2-1,text1,text2,dp);
+
+        return dp[index1][index2]=max(solve(index1-1,index2,text1,text2,dp),solve(index1,index2-1,text1,text2,dp));
+    }
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n1=text1.size();
-        int n2=text2.size();
-        vector<int>prev(n2+1,0);
-        //  lets do space optimization 
-        for(int i=1;i<=n1;i++){
-            vector<int>curr(n2+1,0);
-            for(int j=1;j<=n2;j++){
-                if(text1[i-1]==text2[j-1]){   // we have done the coordinate shifting 
-                    curr[j]=1+prev[j-1];
-                }
-                else{
-                    curr[j]=max(prev[j],curr[j-1]);
-                }
-            }
-            prev=curr;
-        }
+        // the very basic rule of writing the recurrence relation is firstluu 
+        //  express everything in terms of index 
+        //  explore every  possiblity 
+        //  choose the best 
+        //  optimize the memization 
+        vector<vector<int>>dp(text1.size(),vector<int>(text2.size(),-1));
+        return solve(text1.size()-1,text2.size()-1,text1,text2,dp);
 
-        return prev[n2];
+        for(int index1=text1.size()-1;index1>=0;index1--){
+            for(int index2=text2.size()-1;index2>=0;index2--){
+            if(text1[index1]==text2[index2]){
+                dp[index1][index2]=1+dp[index1-1][index2-1];
+            } 
+            else{ 
+            dp[index1][index2]=max(dp[index1-1][index2],dp[index1][index2-1]);
+            }
+
+            }
+        }
+        return dp[0][0];
     }
 };
