fd90de838f085e2f3c6988be4eb73e8fc2c06b19
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-04-03 00:46:59 +0530

    Time: 15 ms (90.25%), Space: 9 MB (98.89%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -15,20 +15,19 @@ public:
         //  explore every  possiblity 
         //  choose the best 
         //  optimize the memization 
-        vector<vector<int>>dp(text1.size(),vector<int>(text2.size(),-1));
-        return solve(text1.size()-1,text2.size()-1,text1,text2,dp);
-
-        for(int index1=text1.size()-1;index1>=0;index1--){
-            for(int index2=text2.size()-1;index2>=0;index2--){
-            if(text1[index1]==text2[index2]){
-                dp[index1][index2]=1+dp[index1-1][index2-1];
-            } 
-            else{ 
-            dp[index1][index2]=max(dp[index1-1][index2],dp[index1][index2-1]);
-            }
-
+         int m = text1.size(), n = text2.size();
+    vector<int> prev(n, 0), curr(n, 0);
+    
+    for (int index1 = m - 1; index1 >= 0; index1--) {
+        for (int index2 = n - 1; index2 >= 0; index2--) {
+            if (text1[index1] == text2[index2]) {
+                curr[index2] = 1 + (index1 + 1 < m && index2 + 1 < n ? prev[index2 + 1] : 0);
+            } else {
+                curr[index2] = max((index1 + 1 < m ? prev[index2] : 0), (index2 + 1 < n ? curr[index2 + 1] : 0));
             }
         }
-        return dp[0][0];
+        prev = curr; // Move current row to previous row
+    }
+    return prev[0];
     }
 };
