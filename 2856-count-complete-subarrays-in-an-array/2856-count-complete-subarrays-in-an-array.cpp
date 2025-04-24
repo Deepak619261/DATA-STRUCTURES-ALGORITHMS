c85fe2e5ed195fa1647f4d9fe84fc03ab10a1e48
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-04-25 00:18:24 +0530

    Time: 11 ms (92.24%), Space: 41.9 MB (59.44%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -6,12 +6,24 @@ public:
         // unordered_map<int,int>mpp;
         int count=0;
 
-        for(int i=0;i<nums.size();i++){
-            unordered_set<int>st2;
-            for(int j=i;j<nums.size();j++){
-               st2.insert(nums[j]);
-               if(st2.size()==st.size())count++;
+        int k=st.size();
+        int first=0;
+        int second=0;
+
+        int n=nums.size();
+        unordered_map<int,int>mpp;
+
+        while(first<n && second<n){
+            mpp[nums[first]]++;
+
+            while(mpp.size()==k){
+                 count+=nums.size()-first;
+                 mpp[nums[second]]--;
+                 if(mpp[nums[second]]==0)mpp.erase(nums[second]);
+                 second++;
             }
+            first++;
+             
         }
 
 
