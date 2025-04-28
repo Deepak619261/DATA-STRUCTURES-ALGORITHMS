d15978ef676a1a32e5451b40123fc48116b0207f
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-04-28 21:38:47 +0530

    Time: 25 ms (4.32%), Space: 9.1 MB (57.27%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,37 +1,58 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        bool isNegative=false;
-        int first=0;
-       
-        
 
-        //  go untill you find a digit 
+        long long answer=0;
 
-        while(isspace(s[first])){
-            first++;
+        int i=0;
+        // remove the whitespace 
+        while(i<s.length() && (s[i]==' ')){
+             i++;
         }
-        if(s[first]=='-' || s[first]=='+'){
-            if(s[first]=='-') isNegative=true;
-            first++;
+
+        // sign handled 
+        bool ispos=true;
+        if(i<s.length()){
+            if(s[i]=='-'){
+                ispos=false;
+                i++;
+            }
+            else if(s[i]=='+')i++;
         }
-        int i=first;
+
+        if(s[i]=='+' || s[i]=='-')return  0;
+
+
+        while(i<s.length() && (s[i]=='0')){
+             i++;
+        }
 
         
-
-        double ans=0;
         
-
-        while(s[i]>='0' && s[i]<='9' && i<s.size()){
-            ans=ans*10+(s[i]-'0');
+        cout<<i<<endl;
+        while(i<s.length()){
+            if(s[i]>='0' && s[i]<='9'){
+                answer=answer*10+(int)(s[i]-'0');
+                cout<<i<<" ->"<<answer<<endl;
+            }
+            else{
+                if(!ispos)return -answer;
+                return answer;
+            }
+            if(!ispos && -answer<INT_MIN)return INT_MIN;
+            if(ispos && answer>INT_MAX)return INT_MAX;
             i++;
         }
+        // cout<<INT_MIN<<endl;
+        if(!ispos)answer=-answer;
 
-        if(isNegative){
-            ans=-ans;
-        }
-        if(ans>INT_MAX) return INT_MAX;
-        if(ans<INT_MIN) return INT_MIN;
-        return ans;
+        if(answer<INT_MIN)return INT_MIN;
+// cout<<"i am here2"<<endl;
+        if(answer>=INT_MAX)return INT_MAX;
+
+        // cout<<"i am here"<<endl;
+
+        // if(!ispos)return -answer;
+        return answer;
     }
 };
