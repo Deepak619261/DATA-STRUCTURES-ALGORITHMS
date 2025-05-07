b0ca5ed58841307dd19269888be4d75e549cbb21
Deepak ARya <113101133+Deepak619261@users.noreply.github.com>
2025-05-08 00:37:49 +0530

    Time: 20 ms (68.39%), Space: 28.8 MB (84.32%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        //  we will be using a distance array to make sure we are taking the min distance
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-
-
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
         pq.push({0,{0,0}});
 
-
-        int n=moveTime.size();
+        int  n=moveTime.size();
         int m=moveTime[0].size();
-
-        vector<vector<int>>distance(n,vector<int>(m,INT_MAX));
 
         int delrow[]={-1,0,1,0};
         int delcol[]={0,1,0,-1};
 
+        vector<vector<int>>distance(n,vector<int>(m,INT_MAX));
+        distance[0][0]=0;
 
         while(!pq.empty()){
             auto it=pq.top();
@@ -25,12 +21,14 @@ public:
 
             pq.pop();
 
-            if(row==n-1 && col==m-1){
-                return time;
-            }
+            if (time > distance[row][col]) 
+                continue;
 
+            if(col==m-1 && row==n-1)return time;
 
-            for(int i=0;i<4;i++){
+            // row+1,col
+            // row,col+1
+           for(int i=0;i<4;i++){
                 int nrow=row+delrow[i];
                 int ncol=col+delcol[i];
 
@@ -43,14 +41,10 @@ public:
                     }
                 }
             }
-
-
-
         }
 
-
         return -1;
-
+        
         
     }
 };
