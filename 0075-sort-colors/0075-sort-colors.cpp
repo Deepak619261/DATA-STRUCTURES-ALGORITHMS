b7c45c1c00d5385040cf24998077cf97985cef3e
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-05-17 22:35:24 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int mid=0;
+        // so whenever we got something like that we have to group the same thing together , we the idea dutch flag algorithm comes , here we have to consider three pointers, first , second , third 
+        // the dutch flag algo says that we want to have the array someting like this , 0 to low-1 , low to mid-1 and high+1 to n-1 
+        // remmeber it 
         int low=0;
         int high=nums.size()-1;
+        int mid=0;
 
         while(mid<=high){
             if(nums[mid]==0){
-                swap(nums[mid++],nums[low++]);
+                swap(nums[low++],nums[mid++]);
             }
             else if(nums[mid]==1){
                 mid++;
             }
             else{
-                swap(nums[high--],nums[mid]);
+                swap(nums[mid],nums[high--]);
             }
         }
     }
