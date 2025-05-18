1b8ff0c4de577b7f6943c22bb74130e2570db630
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-05-18 11:56:23 +0530

    Time: 0 ms (100%), Space: 9.3 MB (56.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,7 +13,28 @@ class Solution {
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(0,0,m,n,dp);
+        // memoization introduced 
+        // lets move to the tabulation 
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[m-1][n-1]=1;
+
+        for(int row=m-1;row>=0;row--){
+            for(int col=n-1;col>=0;col--){
+                if(row==m-1 && col==n-1)continue;
+                int down=0;
+                int right=0;
+                if(row+1<m){
+                    down=dp[row+1][col];
+                }
+                if(col+1<n){
+                    right=dp[row][col+1];
+                }
+
+                dp[row][col]=down+right;
+            }
+        }
+
+
+        return dp[0][0];
     }
 };
