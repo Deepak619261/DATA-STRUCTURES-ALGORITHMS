027c92ff4e8e85e3560384f7ea1194252f2067d3
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-06-24 01:05:36 +0530

    Time: 0 ms (100%), Space: 8.6 MB (35.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -14,7 +14,24 @@ class Solution {
 public:
     int climbStairs(int n) {
         
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        vector<int>dp(n+1,0);
+       
+
+        //  this was the top down approach ,why because we going to base case from the bigger problems 
+        // now we will see the bottom up appraoch 
+       dp[0]=1;
+
+       for(int i=1;i<=n;i++){
+         int onestep=dp[i-1];
+         int twostep=0;
+         if(i>=2){
+            twostep=dp[i-2];
+         }
+
+         dp[i]=onestep+twostep;
+       }
+
+       return dp[n];
+
     }
 };
