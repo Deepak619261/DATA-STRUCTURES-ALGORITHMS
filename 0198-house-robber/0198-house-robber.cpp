f60ec3b7f1efeb931c1e2f4c05b423487bd37575
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-06-27 00:37:00 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,9 +13,22 @@ public:
     int rob(vector<int>& nums) {
         if(nums.size()==1)return nums[0];
 
-        vector<int>dp(nums.size(),-1);
+        vector<int>dp(nums.size()+1,0);
+
+        for(int i=nums.size()-1;i>=0;i--){
+            int pickhouse=nums[i];
+            int dontpickhouse=0;
+            if(i+2<nums.size()){
+               pickhouse+=dp[i+2];
+            }
+            if(i+1<nums.size()){
+               dontpickhouse=dp[i+1];
+            }
+
+            dp[i]=max(dontpickhouse,pickhouse);
+        }
         
-        return solve(0,nums,dp);
+        return dp[0];
 
        
     }
