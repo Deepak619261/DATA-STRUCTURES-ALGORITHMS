021f3ad30487a76d0993d830576fcff382814dfc
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-06-27 00:26:25 +0530

    Time: 0 ms (100%), Space: 10.6 MB (63.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,29 +4,19 @@ class Solution {
 
         if(dp[index]!=-1)return dp[index];
 
-        int takethatcell=nums[index]+solve(index+2,nums,dp);
+        int pickhouse=nums[index]+solve(index+2,nums,dp);
+        int dontpickhouse=solve(index+1,nums,dp);
 
-        int nottakethatcell=solve(index+1,nums,dp);
-
-        return dp[index]=max(takethatcell,nottakethatcell);
+        return dp[index]=max(pickhouse,dontpickhouse);
     }
 public:
     int rob(vector<int>& nums) {
+        if(nums.size()==1)return nums[0];
+
+        vector<int>dp(nums.size(),-1);
         
-        
-        int next2=0;
-        int next1=0;
-        int curr=0;
+        return solve(0,nums,dp);
 
-        for(int i=nums.size()-1;i>=0;i--){
-            int takethatcell=nums[i]+next2;
-            int nottakethatcell=next1;
-
-            curr=max(takethatcell,nottakethatcell);
-            next2=next1;
-            next1=curr;
-        }
-
-        return curr;
+       
     }
 };
