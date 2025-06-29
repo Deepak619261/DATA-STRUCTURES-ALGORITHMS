9fbdd6fee56a19304b0d802e678f668a59d2dc31
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-06-30 00:01:04 +0530

    Time: 3 ms (66.79%), Space: 8.4 MB (11.05%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -8,8 +8,17 @@ class Solution {
     }
 public:
     int fib(int n) {
-       vector<int>dp(n+1,-1);
-       return solve(n,dp);
+    if(n<=1)return n;
+    vector<int>dp(n+1,-1);
+    //    return solve(n,dp);
+    dp[0]=0;
+    dp[1]=1;
+
+       for(int index=2;index<=n;index++){
+          dp[index]=dp[index-1]+dp[index-2];
+       }
+
+    return dp[n];
 
         
     }
