fd17f2fdf842bd223aba367a2461697068a94285
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-07-04 01:00:55 +0530

    Time: 3 ms (57.81%), Space: 9.8 MB (63.36%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,27 +1,23 @@
 class Solution {
-    void solve(string &ans,int k){
-        if(ans.size()>=k){
-            return;
-        }
-
-        int len=ans.size();
-
-        for(int i=0;i<len;i++){
-            if(ans[i]=='z'){
-                ans+='a';
-            }
-            else{
-                char ch=ans[i];
-                ch=ch+1;
-                ans+=ch;
-            }
-        }
-        solve(ans,k);
-    }
 public:
     char kthCharacter(int k) {
-        string ans="a";
-        solve(ans,k);
-        return ans[k-1];
+        string str="a";
+
+        while(str.size()<k){
+            //  take the current string add +1 to it 
+            string toadd=str;
+            for(int i=0;i<toadd.size();i++){
+                if(toadd[i]=='z'){
+                    toadd[i]='a';
+                }
+                else{
+                    toadd[i]=toadd[i]+1;
+                }
+            }
+            str+=toadd;
+        }
+
+        cout<<str<<endl;
+        return str[k-1];
     }
 };
