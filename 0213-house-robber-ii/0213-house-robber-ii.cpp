28f9a354b5d25ffc329feffd0b98d9a22c540f25
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-07-28 08:24:45 +0530

    Time: 0 ms (100%), Space: 11 MB (20.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,32 +1,42 @@
 class Solution {
-int maximumNonAdjacentSum(vector<int> &nums){
-    // Write your code here.
-    vector<int>dp(nums.size()+2,-1);
+    int solve(int index, vector<int>&nums,int n,vector<int>&dp){
+        if(index>=n)return 0;
 
-    dp[nums.size()]=0;
-    dp[nums.size()+1]=0;
-    
-    for(int i=nums.size()-1;i>=0;i--){
-        int takesum=nums[i]+dp[i+2];
-        int nottakesum=dp[i+1];
-        dp[i]=max(takesum,nottakesum);
+
+        if(dp[index]!=-1)return dp[index];
+
+        // we got two option either pick the current house and move index+2 
+        // or don't pick and just move forward 
+        int pick=nums[index]+solve(index+2,nums,n,dp);
+
+        int dontpick=solve(index+1,nums,n,dp);
+
+        return dp[index]=max(pick,dontpick);
     }
-    return dp[0];
-}
 public:
     int rob(vector<int>& nums) {
-          if(nums.size()==1)return nums[0];
+        // there will be the flow same as the house robber i but we will check the two config , one is that we will start from 0 and second we will start from 1 
+        int n=nums.size();
+        if(n==1)return nums[0];
+        vector<int>dp1(n+1,0);
+        vector<int>dp2(n+2,0);
 
-           vector<int>arr1;
-           vector<int>arr2;
-           for(int i=1;i<nums.size();i++){
-            arr1.push_back(nums[i]);
-           }
+        // first loop with dp 1 
+        for(int i=n-2;i>=0;i--){
+            int pick=nums[i]+dp1[i+2];
+            int dontpick=dp1[i+1];
+            dp1[i]=max(pick,dontpick);
+        }
 
-           for(int i=0;i<nums.size()-1;i++){
-             arr2.push_back(nums[i]);
-           }
+        for(int i=n-1;i>=1;i--){
+            int pick=nums[i]+dp2[i+2];
+            int dontpick=dp2[i+1];
+            dp2[i]=max(pick,dontpick);
+        }
 
-           return max(maximumNonAdjacentSum(arr1),maximumNonAdjacentSum(arr2));
+
+        return max(dp1[0],dp2[1]);
+
+       
     }
 };
