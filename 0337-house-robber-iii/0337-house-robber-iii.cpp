852181c149cd53f435c3f8d60a31ed5aeaad0397
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-07-30 00:13:33 +0530

    Time: 31 ms (5.41%), Space: 26.8 MB (25.25%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -10,31 +10,27 @@
  * };
  */
 class Solution {
-    int solve(TreeNode* root, bool canROB,map<pair<TreeNode*,bool>,int>&dp){
-        if(root==NULL)return 0;
+    int solve(TreeNode* root,map<TreeNode*,int>&dp){
+        if(root==NULL)return 0; 
 
-        int rob=0;
-        int dontrob=0;
+        //  at every node we got two choice either loot that node or not 
+        int dontrob=solve(root->left,dp)+solve(root->right,dp);
+        int rob=root->val;
 
-        pair<TreeNode*,bool>key={root,canROB};
-        if(dp.find(key)!=dp.end())return dp[key];
+        if(dp.find(root)!=dp.end())return dp[root];
 
-        if(canROB){
-            rob=root->val+solve(root->left,false,dp)+solve(root->right,false,dp);
-            dontrob=solve(root->left,true,dp)+solve(root->right,true,dp);
+        if(root->left){
+            rob+=solve(root->left->left,dp)+solve(root->left->right,dp);
         }
-        else{
-            rob=solve(root->left,false,dp)+solve(root->right,false,dp);
-            dontrob=solve(root->left,true,dp)+solve(root->right,true,dp);
-
+        if(root->right){
+            rob+=solve(root->right->left,dp)+solve(root->right->right,dp);
         }
 
-        return dp[key]=max(rob,dontrob);
-
+        return dp[root]=max(dontrob,rob);
     }
 public:
     int rob(TreeNode* root) {
-        map<pair<TreeNode*,bool>,int>dp;
-        return max(solve(root,true,dp),solve(root,false,dp));
+        map<TreeNode*,int>dp;
+        return solve(root,dp);
     }
 };
