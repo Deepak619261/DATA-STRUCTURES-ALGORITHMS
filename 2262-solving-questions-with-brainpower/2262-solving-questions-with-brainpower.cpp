f95d8c14a68c82269230606646950704370e099e
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-07-31 21:54:41 +0530

    Time: 0 ms (100%), Space: 119.6 MB (88.67%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -15,7 +15,16 @@ class Solution {
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         int n=questions.size();
-        vector<long long>dp(n,-1);
-        return solve(0,questions,dp);
+        vector<long long>dp(n+1,0);
+
+        for(int i=n-1;i>=0;i--){
+            long long pick=questions[i][0];
+            if(i+questions[i][1]+1<=n){
+                pick+=dp[i+questions[i][1]+1];
+            }
+            long long dontpick=dp[i+1];
+            dp[i]=max(pick,dontpick);
+        }
+        return dp[0];
     }
 };
