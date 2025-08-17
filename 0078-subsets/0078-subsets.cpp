9eebfaa1dc5c109225d3550f926c14f903dda58f
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-08-17 18:52:58 +0530

    Time: 3 ms (31.79%), Space: 9.8 MB (80.99%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -13,11 +13,29 @@ class Solution {
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        //  lets go with very straight forward 
+        //  second approach is via bit manipulation 
+        //  there will be subsets 2^n with the empty set including 
+        int n =nums.size();
+        int count=pow(2,n);
+
+        
         vector<vector<int>>ans;
 
+        for(int i=0;i<count;i++){
+            // now we are getting something like 
+            //  000,001, 010,011,100,101,110,111
+            //  we have to see the indices of 1's in every num and pick that one number only 
+            //  what we will do , we'll take the 1 and we will left shit it number of times of digits presents 
+            vector<int>temp;
+            for(int j=0;j<n;j++){
 
-        solve(0,ans,{},nums);
+                if(i & (1<<j)){
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+        }
+
         return ans;
     }
 };
