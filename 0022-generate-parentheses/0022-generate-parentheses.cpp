8a072a3c93ea96053c6c06fd75e5d57cb4fec821
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-08-17 18:28:56 +0530

    Time: 3 ms (71.59%), Space: 12.1 MB (95.82%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,22 +1,43 @@
 class Solution {
-    void solve(int count, vector<string>&ans, int openP,int closeP,string str,int n){
-        if(openP>n || closeP>n || closeP>openP)return;
-
-        if(count==2*n){
-            ans.push_back(str);
-            return;
-        }
-        
-        //  at every step we only have two options either we choose a openP or a closeP
-        solve(count+1,ans,openP+1,closeP,str+"(",n);
-        solve(count+1,ans,openP,closeP+1,str+")",n);
+   void solve(int open,int close, vector<string>&result,string &str){
+    if(open==0 && close==0){
+        result.push_back(str);
+        return;
     }
+
+    if(close==open){
+        str+='(';
+        solve(open-1,close,result,str);
+        str.pop_back();
+    }
+    else if(open==0){
+        str+=')';
+        solve(open,close-1,result,str);
+        str.pop_back();
+    }
+    else if(close==0){
+        str+='(';
+        solve(open-1,close,result,str);
+        str.pop_back();
+    }
+    else{
+        str+='(';
+        solve(open-1,close,result,str);
+        str.pop_back();
+
+        str+=')';
+        solve(open,close-1,result,str);
+        str.pop_back();
+    }
+   }
 public:
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
-        string curr="(";
-        solve(1,ans,1,0,curr,n);
-
+        int opencnt=n;
+        int closecnt=n;
+        string str="";
+        solve(opencnt,closecnt,ans,str);
         return ans;
+
     }
 };
