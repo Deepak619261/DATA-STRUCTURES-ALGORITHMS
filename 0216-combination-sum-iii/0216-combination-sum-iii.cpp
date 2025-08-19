188e57d4f25e13035f66f1e2147ebc8dc5c33bfc
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-08-19 07:52:20 +0530

    Time: 7 ms (5.53%), Space: 14.7 MB (5.81%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,29 +1,32 @@
 class Solution {
-    void solve(int index,int k, int n, vector<vector<int>>&ans,vector<int>store){
-        if(index==k+1){
-            if(n==0){
-                ans.push_back(store);
-            }
+    void solve(int index,int count, vector<vector<int>>&ans,vector<int>temp,vector<int>&nums,int sum,int k,int n){
+        
+         if(count==k){
+            cout<<sum<<endl;
+            if(sum==n)ans.push_back(temp);
             return;
-        }
+         }
+         if(index==nums.size()){
+            if(count==k){
+            if(sum==n)ans.push_back(temp);
+         }
+         return;
+         }
 
-
-        for(int i=index;i<=9;i++){
-            if(!store.empty() && i<=store.back()) continue;
-
-            store.push_back(i);
-            n-=i;
-            solve(index+1,k,n,ans,store);
-            n+=i;
-            store.pop_back();
-        }
+        //   on every index we have two choices either we pick the current index or we don't 
+        //  first lets pick 
+        temp.push_back(nums[index]);
+        solve(index+1, count+1,ans,temp,nums,sum+nums[index],k,n);
+        temp.pop_back();
+        solve(index+1,count,ans,temp,nums,sum,k,n);
     }
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>>ans;
-        vector<int>store;
-        solve(1,k,n,ans,store);
-
+        vector<int>nums={1,2,3,4,5,6,7,8,9};
+        vector<int>temp;
+        solve(0,0,ans,temp,nums,0,k,n);
         return ans;
+
     }
 };
