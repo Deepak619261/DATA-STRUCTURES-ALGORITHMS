f96e2e93d4e05662c5cae6bbe75664fa3f1396ed
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-08-19 08:36:14 +0530

    Time: 0 ms (100%), Space: 9.4 MB (22.44%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,32 +1,24 @@
 class Solution {
-    void solve(int index,int count, vector<vector<int>>&ans,vector<int>temp,vector<int>&nums,int sum,int k,int n){
-        
-         if(count==k){
-            cout<<sum<<endl;
+    void solve(int index, vector<vector<int>>&ans,vector<int>temp,int sum , int k , int n ){
+        if(temp.size()==k){
             if(sum==n)ans.push_back(temp);
             return;
-         }
-         if(index==nums.size()){
-            if(count==k){
-            if(sum==n)ans.push_back(temp);
-         }
-         return;
-         }
+        }
 
-        //   on every index we have two choices either we pick the current index or we don't 
-        //  first lets pick 
-        temp.push_back(nums[index]);
-        solve(index+1, count+1,ans,temp,nums,sum+nums[index],k,n);
-        temp.pop_back();
-        solve(index+1,count,ans,temp,nums,sum,k,n);
+        //  loop from the index to  9 
+
+        for(int i=index;i<10;i++){
+            if(sum+i>n)break;
+            temp.push_back(i);
+            solve(i+1,ans,temp,sum+i,k,n);
+            temp.pop_back();
+        }
     }
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>>ans;
-        vector<int>nums={1,2,3,4,5,6,7,8,9};
         vector<int>temp;
-        solve(0,0,ans,temp,nums,0,k,n);
+        solve(1,ans,temp,0,k,n);
         return ans;
-
     }
 };
