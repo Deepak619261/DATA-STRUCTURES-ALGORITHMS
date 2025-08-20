77e9be2e4fa770464fe589f79e42cd59c297e601
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-08-20 21:44:42 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.52%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,38 +1,25 @@
 class Solution {
-    void solve(int index,vector<string>&ans,string digits,vector<string>&dict,string curr){
-        if(index==digits.size()){
-            if(!curr.empty()){
-            ans.push_back(curr);
-            }
-            return;
-        }
-
-        // lets say i am at the first index 
-        for(int i=0;i<dict[digits[index]-'0'].size();i++){
-            solve(index+1,ans,digits,dict,curr+dict[digits[index]-'0'][i]);
-        }
+    void solve(int index, vector<string>&ans,vector<string>&mpp , string str, string digits){
+    if(index==digits.size()){
+      ans.push_back(str);
+      return;
     }
+
+int x=digits[index]-'0';
+
+    for(int i=0;i<mpp[x].size();i++){
+       solve(index+1,ans,mpp,str+mpp[x][i],digits);
+    }
+
+}
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string>ans;
-        // create the mapping 
-        vector<string>dict(10);
-        dict.push_back("");
+         //declare a vector<string 
+         vector<string>mpp={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
-        dict[2]="abc";
-        dict[3]="def";
-        dict[4]="ghi";
-        dict[5]="jkl";
-        dict[6]="mno";
-        dict[7]="pqrs";
-        dict[8]="tuv";
-        dict[9]="wxyz";
-
-        solve(0,ans,digits,dict,"");
-
-        return ans;
-    
-       
-        
+         vector<string>ans;
+        if(digits.empty())return ans;
+         solve(0,ans,mpp,"",digits);
+         return ans;
     }
 };
