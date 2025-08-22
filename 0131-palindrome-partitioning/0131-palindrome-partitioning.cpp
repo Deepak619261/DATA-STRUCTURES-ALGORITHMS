b296bdbd9661c752b4ffb6272739c9b2bffeb779
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-08-23 00:17:30 +0530

    Time: 154 ms (7.86%), Space: 148.9 MB (9.14%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,30 @@
 class Solution {
-    void solve(vector<vector<string>>&ans,vector<string>store,string &s, int index){
+    bool isPalindrome(string str, int s , int e){
+        while(s<=e){
+            if(str[s++]!=str[e--])return false;
+        }
+        return true;
+    }
+    void solve(int index, vector<vector<string>>&ans, vector<string>temp,string s){
         if(index==s.size()){
-            ans.push_back(store);
+            ans.push_back(temp);
             return;
         }
 
         for(int i=index;i<s.size();i++){
-            if(ispalindrom(s,index,i)){
-                store.push_back(s.substr(index,i-index+1));
-                solve(ans,store,s,i+1);
-                store.pop_back();
+            if(isPalindrome(s,index,i)){
+                temp.push_back(s.substr(index,i-index+1));
+                solve(i+1,ans,temp,s);
+                temp.pop_back();
             }
         }
     }
-
-    bool ispalindrom(string str,int i, int j){
-        while(i<=j){
-            if(str[i]!=str[j]) return false;
-            i++;
-            j--;
-        }
-
-        return true;
-    }
 public:
     vector<vector<string>> partition(string s) {
+        //  brute force way is to check every single substring if its the palindrom or not 
         vector<vector<string>>ans;
-        vector<string>store;
-        solve(ans,store,s,0);
+        vector<string>temp;
+        solve(0,ans,temp,s);
         return ans;
     }
 };
