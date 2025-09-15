490290ea0f3418ee6b8c71abb7e7af34bae65f44
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-09-15 22:50:51 +0530

    Time: 7 ms (21.22%), Space: 15.4 MB (13.3%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -25,16 +25,22 @@ class Solution {
         return true;
 
     }
-    void solve(int row, int col,vector<string>&board,vector<vector<string>>&ans,int n){
+    void solve(int row, int col,vector<string>&board,vector<vector<string>>&ans,int n,vector<int>leftrow,vector<int>lowerdiag,vector<int>upperdiag){
         if(col==n){
             ans.push_back(board);
             return;
         }
 
         for(int row=0;row<n;row++){
-            if(isvalid(row,col,board,n)){
+            if(leftrow[row]==0 && lowerdiag[row+col]==0 && upperdiag[n-1+col-row]==0){
                 board[row][col]='Q';
-                solve(row,col+1,board,ans,n);
+                leftrow[row]=1;
+                lowerdiag[row+col]=1;
+                upperdiag[n-1+col-row]=1;
+                solve(row,col+1,board,ans,n,leftrow,lowerdiag,upperdiag);
+                leftrow[row]=0;
+                lowerdiag[row+col]=0;
+                upperdiag[n-1+col-row]=0;
                 board[row][col]='.';
             }
         }
@@ -48,7 +54,15 @@ public:
         for(int i=0;i<n;i++){
             board[i]=temp;
         }
-        solve(0,0,board,ans,n);
+        vector<int>leftrow(n,0);
+        vector<int>lowerdiag(2*n-1,0);
+        vector<int>upperdiag(2*n-1,0);
+        solve(0,0,board,ans,n,leftrow,lowerdiag,upperdiag);
+
+        //  optimizing the solution by hashing 
+        // the formula we have to remember 
+        //  for left lower diagonal -> row+col 
+        // for upper diagonal -> n-1+col-row
         return ans;
     }
 };
