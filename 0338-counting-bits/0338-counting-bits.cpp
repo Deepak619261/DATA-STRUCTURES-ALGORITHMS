bfc877d9757681d5b149933475c5129e7c896c31
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2025-10-30 16:33:43 +0530

    Time: 3 ms (24.47%), Space: 10.5 MB (80.11%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+
+ int calculateones(int n){
+        int count=0;
+        while(n){
+           if(n&1){
+            count++;
+           }
+           n=n>>1;
+        }
+        return count;
+    }
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans(n+1,0);
+
+        for(int i=0;i<=n;i++){
+            ans[i]=calculateones(i);
+        }
+
+        return ans;
+
+    }
+};
