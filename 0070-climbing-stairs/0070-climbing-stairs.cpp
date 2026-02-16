41a72b163d78558477d17106589227adf3839046
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2026-02-16 23:04:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (34.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,42 +1,14 @@
 class Solution {
-    int solve(int n,vector<int>&dp){
-        if(n<=0)return 1;
-
+    int solve(int n , vector<int>&dp){
+        if(n==0)return 1;
+        if(n<0)return 0;
         if(dp[n]!=-1)return dp[n];
-
-        int onestep=solve(n-1,dp);
-        int twostep=0;
-        if(n>=2)twostep=solve(n-2,dp);
-
-        return dp[n]=onestep+twostep;
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
     }
-    
 public:
     int climbStairs(int n) {
+      vector<int>dp(n+1,-1);
+      return solve(n,dp);
         
-        vector<int>dp(n+1,0);
-       
-
-        //  this was the top down approach ,why because we going to base case from the bigger problems 
-        // now we will see the bottom up appraoch 
-        // space optimization 
-       int prev=1;
-       int prev2=1;
-
-       for(int i=1;i<=n;i++){
-        int curri=0;
-         int onestep=prev;
-         int twostep=0;
-         if(i>=2){
-            twostep=prev2;
-         }
-
-         curri=onestep+twostep;
-         prev2=prev;
-         prev=curri;
-       }
-
-       return prev;
-
     }
 };
