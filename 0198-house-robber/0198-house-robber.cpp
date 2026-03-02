27f5645c8a297c540576e07a5d99f54ba1f6e2d6
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2026-03-02 23:32:38 +0530

    Time: 0 ms (100%), Space: 10.6 MB (61.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,39 +2,22 @@ class Solution {
     int solve(int index, vector<int>&nums,vector<int>&dp){
         if(index>=nums.size())return 0;
 
+
         if(dp[index]!=-1)return dp[index];
 
-        int pickhouse=nums[index]+solve(index+2,nums,dp);
-        int dontpickhouse=solve(index+1,nums,dp);
+        // we should not use the INT_MIN as in any case it can just pick the negative value in max value picking so that case 
+        // INT_MIN + some negative value --> will cause the overflow 
 
-        return dp[index]=max(pickhouse,dontpickhouse);
+        //  it can have two choice , it can pick the current one and move index+2 ,
+        // or it just don't pick the one and just move one ahead index+1 
+        int pick=nums[index]+solve(index+2,nums,dp);
+        int dontpick=solve(index+1,nums,dp);
+
+        return dp[index]=max(pick,dontpick);
     }
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()==1)return nums[0];
-
-        // vector<int>dp(nums.size()+1,0);
-        int next1=0;
-        int next2=0;
-
-        for(int i=nums.size()-1;i>=0;i--){
-            int curri=0;
-            int pickhouse=nums[i];
-            int dontpickhouse=0;
-            if(i+2<nums.size()){
-               pickhouse+=next2;
-            }
-            if(i+1<nums.size()){
-               dontpickhouse=next1;
-            }
-
-            curri=max(dontpickhouse,pickhouse);
-            next2=next1;
-            next1=curri;
-        }
-        
-        return next1;
-
-       
+        vector<int>dp(nums.size(),-1);
+        return max(solve(0,nums,dp),solve(1,nums,dp));
     }
 };
