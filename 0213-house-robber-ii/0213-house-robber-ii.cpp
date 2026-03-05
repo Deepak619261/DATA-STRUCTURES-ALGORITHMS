5e757adc5ef8d8c443e6e555a3eb30d4418630bb
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2026-03-05 22:51:26 +0530

    Time: 14 ms (0.5%), Space: 10.9 MB (38.02%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,52 +1,26 @@
 class Solution {
-    int solve(int index, vector<int>&nums,int n,vector<int>&dp){
-        if(index>=n)return 0;
+       int solve(int index, vector<int>&nums,vector<int>&dp,int size){
+        if(index>=size)return 0;
 
 
         if(dp[index]!=-1)return dp[index];
 
-        // we got two option either pick the current house and move index+2 
-        // or don't pick and just move forward 
-        int pick=nums[index]+solve(index+2,nums,n,dp);
+        // we should not use the INT_MIN as in any case it can just pick the negative value in max value picking so that case 
+        // INT_MIN + some negative value --> will cause the overflow 
 
-        int dontpick=solve(index+1,nums,n,dp);
-
+        //  it can have two choice , it can pick the current one and move index+2 ,
+        // or it just don't pick the one and just move one ahead index+1 
+        int pick=nums[index]+solve(index+2,nums,dp,size);
+        int dontpick=solve(index+1,nums,dp,size);
+        
+        cout<<"size is "<<size<<" and the value is"<<max(pick,dontpick)<<endl;
         return dp[index]=max(pick,dontpick);
     }
 public:
     int rob(vector<int>& nums) {
-        // there will be the flow same as the house robber i but we will check the two config , one is that we will start from 0 and second we will start from 1 
-        int n=nums.size();
-        if(n==1)return nums[0];
-        // vector<int>dp1(n+1,0);
-        vector<int>dp2(n+2,0);
-        int next2=0;
-        int next1=0;
-        // first loop with dp 1 
-        for(int i=n-2;i>=0;i--){
-            int pick=nums[i]+next2;
-            int dontpick=next1;
-            int curri=max(pick,dontpick);
-
-            next2=next1;
-            next1=curri;
-        }
-
-
-        int nexi2=0;
-        int nexi1=0;
-
-        for(int i=n-1;i>=1;i--){
-            int pick=nums[i]+nexi2;
-            int dontpick=nexi1;
-            int curri=max(pick,dontpick);
-            nexi2=nexi1;
-            nexi1=curri;
-        }
-
-
-        return max(next1,nexi1);
-
-       
+        if(nums.size()==1)return nums[0];
+        vector<int>dp(nums.size()+1,-1);
+        vector<int>dp2(nums.size()+1,-1);
+        return max(solve(0,nums,dp,nums.size()-1),solve(1,nums,dp2,nums.size()));
     }
 };
