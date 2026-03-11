5ae5ea484c9ff85eb8afbc9fb1e3729b7d3162a0
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2026-03-12 01:13:04 +0530

    Time: 81 ms (50.36%), Space: 31.8 MB (46.44%) - LeetHub

---
+++ 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int n=s.size();
+        string str=s;
+        sort(str.begin(),str.end());
+        if(s==str)return 0;
+
+        if(n==2){
+            if(s==str)return 0;
+            return -1;
+        }
+        
+        if(str[0]==s[0] || s[n-1]==str[n-1])return 1;
+
+        // if(str[0]==s[n-1] && str[n-1]==s[0])return 3;
+        char mn=str[0];
+        char mx=str[n-1];
+
+        for(int i=0;i<s.size();i++){
+             if(s[i]==mn && i!=n-1)return 2;
+             if(s[i]==mx && i!=0)return 2;
+        }
+
+        return 3;
+    }
+};
