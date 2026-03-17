6bc62a8b845036a04cfda220f86c474324f784ef
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2026-03-18 00:59:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (80.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,28 +13,29 @@ class Solution {
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[m-1][n-1]=1;
+        // vector<vector<int>>dp(m,vector<int>(n,0));
+        vector<int>below(n,0);
+        below[n-1]=1;
 
         for(int i=m-1;i>=0;i--){
+            vector<int>curr(n,0);
             for(int j=n-1;j>=0;j--){
-                if(i==m-1 && j==n-1)continue;
+                if(i==m-1 && j==n-1){
+                    curr[j]=1;
+                    continue;
+                }
                 int down=0;
                 int right=0;
-                if(i+1<=m-1)down+=dp[i+1][j];
-                if(j+1<=n-1)right+=dp[i][j+1];
+                if(i+1<=m-1)down+=below[j];
+                if(j+1<=n-1)right+=curr[j+1];
 
-                dp[i][j]=down+right;
+                curr[j]=down+right;
             }
+            below=curr;
         }
 
-        for(int i = 0; i < m; i++){
-    for(int j = 0; j < n; j++){
-        cout << dp[i][j] << " ";
-    }
-    cout << endl;
-}
-        return dp[0][0];
+//    ,
+        return below[0];
         
     }
 };
