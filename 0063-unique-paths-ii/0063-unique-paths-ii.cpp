7563ae8c251c45930577c4d2817a56698e18dc2e
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2026-03-19 00:58:20 +0530

    Time: 0 ms (100%), Space: 11.6 MB (76.92%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -15,33 +15,41 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<long long>>dp(m,vector<long long>(n,0));
-        dp[m-1][n-1]=1;
+        // vector<vector<long long>>dp(m,vector<long long>(n,0));
+        vector<long long>next(n,0);
+        // dp[n-1]=1;
 
         if(obstacleGrid[m-1][n-1]==1)return 0;
         // return solve(0,0,m,n,obstacleGrid,dp);
         for(int i=m-1;i>=0;i--){
+            vector<long long>curri(n,0);
             for(int j=n-1;j>=0;j--){
-                if(i==m-1 && j==n-1)continue;
+                if(i==m-1 && j==n-1){
+                    curri[j]=1;
+                    continue;
+                }
                 long long down=0;
                 long long right=0;
                 if(i+1<m && obstacleGrid[i+1][j]==0 && obstacleGrid[i][j]==0){
-                    down+=dp[i+1][j];
+                    down+=next[j];
                 }
                 if(j+1<n && obstacleGrid[i][j+1]==0 && obstacleGrid[i][j]==0){
-                    right+=dp[i][j+1];
+                    right+=curri[j+1];
                 }
-                dp[i][j]=(long long)(down+right);
+                curri[j]=(long long)(down+right);
             }
+            next=curri;
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
-        }
+        return next[0];
 
-        return dp[0][0];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
+        // return dp[0][0];
     }
 };
