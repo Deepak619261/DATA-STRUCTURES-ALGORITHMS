a85b438033f9754cd9b6f81d94be4cebefe632ea
Deepak kumar <113101133+Deepak619261@users.noreply.github.com>
2026-03-19 00:52:28 +0530

    Time: 19 ms (2.91%), Space: 11.9 MB (53.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -15,7 +15,33 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(0,0,m,n,obstacleGrid,dp);
+        vector<vector<long long>>dp(m,vector<long long>(n,0));
+        dp[m-1][n-1]=1;
+
+        if(obstacleGrid[m-1][n-1]==1)return 0;
+        // return solve(0,0,m,n,obstacleGrid,dp);
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1)continue;
+                long long down=0;
+                long long right=0;
+                if(i+1<m && obstacleGrid[i+1][j]==0 && obstacleGrid[i][j]==0){
+                    down+=dp[i+1][j];
+                }
+                if(j+1<n && obstacleGrid[i][j+1]==0 && obstacleGrid[i][j]==0){
+                    right+=dp[i][j+1];
+                }
+                dp[i][j]=(long long)(down+right);
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+
+        return dp[0][0];
     }
 };
