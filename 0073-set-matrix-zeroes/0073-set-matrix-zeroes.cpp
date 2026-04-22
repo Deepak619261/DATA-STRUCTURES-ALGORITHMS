112328ecc5f9a5a9389ae2fd288218a93c1b66af
Deepak kumar <deepakarya140831@gmail.com>
2026-04-23 02:37:17 +0530

    Time: 0 ms (100%), Space: 20.8 MB (60.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,32 +4,26 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
 
-        vector<int>Rarr(m,0);
-        vector<int>Carr(n,0);
+        vector<bool>rows(m,false);
+        vector<bool>cols(n,false);
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    Rarr[i]=1;
-                    Carr[j]=1;
+                  rows[i]=true;
+                  cols[j]=true;
                 }
             }
         }
 
-        for(int i=0;i<Rarr.size();i++){
-            if(Rarr[i]==1){
-                for(int k=0;k<matrix[i].size();k++){
-                    matrix[i][k]=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(rows[i]==true || cols[j]==true){
+                    matrix[i][j]=0;
                 }
             }
         }
 
-        for(int i=0;i<Carr.size();i++){
-            if(Carr[i]==1){
-                for(int k=0;k<matrix.size();k++){
-                    matrix[k][i]=0;
-                }
-            }
-        }
+        // return matrix;
     }
 };
