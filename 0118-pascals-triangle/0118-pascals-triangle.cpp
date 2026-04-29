e24d0c805e98cb32a6119ba6af13ea2daa896ea0
Deepak kumar <deepakarya140831@gmail.com>
2026-04-29 18:51:51 +0530

    Time: 0 ms (100%), Space: 9.8 MB (29.76%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<int>base(1,1);
+        vector<vector<int>>res;
+        res.push_back(base);
+
+        for(int i=1;i<numRows;i++){
+            vector<int>lastappend=res.back();
+            vector<int>temp(lastappend.size()+1,0);
+
+            //  go from 1 to n-1 so say n=3 so go from 1 to <n-1 that is 1 to <2
+            int n=temp.size();
+            temp[0]=1;
+            temp[n-1]=1;
+
+            for(int i=1;i<n-1;i++){
+                temp[i]=lastappend[i-1]+lastappend[i];
+            }
+            res.push_back(temp);
+            
+        }
+
+        return res;
+    }
+};
