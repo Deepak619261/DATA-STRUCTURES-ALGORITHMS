d597523feedff6037858c338c446d8cfe7be4708
Deepak kumar <deepakarya140831@gmail.com>
2026-04-29 23:51:22 +0530

    Time: 2 ms (15.02%), Space: 9.8 MB (29.76%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,24 +1,48 @@
 class Solution {
+    long long factorial(int row , int col){
+
+        long long fact=1;
+        for(int i=0;i<col;i++){
+            fact=fact*(row-i);
+            fact=fact/(i+1);
+        }
+
+        return fact;
+    }
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<int>base(1,1);
+
+        // vector<int>base(1,1);
+        // vector<vector<int>>res;
+        // res.push_back(base);
+
+        // for(int i=1;i<numRows;i++){
+        //     vector<int>lastappend=res.back();
+        //     vector<int>temp(lastappend.size()+1,0);
+
+        //     //  go from 1 to n-1 so say n=3 so go from 1 to <n-1 that is 1 to <2
+        //     int n=temp.size();
+        //     temp[0]=1;
+        //     temp[n-1]=1;
+
+        //     for(int i=1;i<n-1;i++){
+        //         temp[i]=lastappend[i-1]+lastappend[i];
+        //     }
+        //     res.push_back(temp);
+            
+        // }
+
+        // return res;
         vector<vector<int>>res;
-        res.push_back(base);
 
-        for(int i=1;i<numRows;i++){
-            vector<int>lastappend=res.back();
-            vector<int>temp(lastappend.size()+1,0);
-
-            //  go from 1 to n-1 so say n=3 so go from 1 to <n-1 that is 1 to <2
-            int n=temp.size();
-            temp[0]=1;
-            temp[n-1]=1;
-
-            for(int i=1;i<n-1;i++){
-                temp[i]=lastappend[i-1]+lastappend[i];
+        for(int i=0;i<numRows;i++){
+            vector<int>temp(i+1,1);
+            int col=i+1;
+            for(int j=1;j<=i;j++){
+                temp[j]=factorial(i,j);
             }
             res.push_back(temp);
-            
+
         }
 
         return res;
