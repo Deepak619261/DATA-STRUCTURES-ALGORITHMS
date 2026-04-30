72db6861540e08b3f892ee3f920bc8fb3e905ddb
Deepak kumar <deepakarya140831@gmail.com>
2026-05-01 02:47:43 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.13%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    void solve(vector<vector<int>>&res ,vector<int>temp,int n ,vector<bool>freq,vector<int>nums){
+    void solve(vector<vector<int>>&res ,vector<int>&temp,int n ,vector<bool>&freq,vector<int>&nums){
       if(temp.size()==n)res.push_back(temp);
 
     //    put a for loop 
