6d725aa2f331b3a27892acd9a4447a95cb30c68a
Deepak kumar <deepakarya140831@gmail.com>
2026-05-01 02:42:34 +0530

    Time: 7 ms (6.37%), Space: 13 MB (6.2%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,27 @@
 class Solution {
-    private:
-    void solve(vector<int>nums,vector<int>&ds, vector<vector<int>>&ans,map<int,bool>mpp,int index){
-        if(index==nums.size()){
-            ans.push_back(ds);
-            return;
+    void solve(vector<vector<int>>&res ,vector<int>temp,int n ,vector<bool>freq,vector<int>nums){
+      if(temp.size()==n)res.push_back(temp);
+
+    //    put a for loop 
+    for(int i=0;i<n;i++){
+        if(!freq[i]){
+            temp.push_back(nums[i]);
+            freq[i]=true;
+            solve(res,temp,n,freq,nums);
+            freq[i]=false;
+            temp.pop_back();
         }
-        for(int i=0;i<nums.size();i++){
-            if(!mpp[i]){
-                mpp[i]=true;
-                ds.push_back(nums[i]);
-                solve(nums,ds,ans,mpp,index+1);
-                ds.pop_back();
-                mpp[i]=false;
-            }
-        }
+    }
+
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        //  the new approach according to striver's
-        map<int,bool>mpp;
-        vector<int>ds;
-        vector<vector<int>>ans;
-        solve(nums,ds,ans,mpp,0);
-        return ans;
+        //  lets go with the recursive approach 
+        int n=nums.size();
+        vector<bool>freq(n,false);
+        vector<vector<int>>res;
+        vector<int>temp;
+        solve(res,temp,n,freq,nums);
+        return res;
     }
 };
