4db06b329669314a25e5653560cc362c6399d3d3
Deepak kumar <deepakarya140831@gmail.com>
2026-05-20 01:08:08 +0530

    Time: 0 ms (100%), Space: 9 MB (13.62%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,51 +1,31 @@
 class Solution {
-    string next_permutation(string str){
-        int n=str.size();
-
-        if(n==1)return str;
-        int ind=-1;
-
-
-
-        for(int i=n-2;i>=0;i--){
-            int curr=str[i]-'0';
-            int nexi=str[i+1]-'0';
-
-            if(curr<nexi){
-                ind=i;
-                break;
-            }
-        }
-
-        if(ind==-1)return str;
-
-        int pivot=str[ind]-'0';
-
-        for(int i=n-1;i>=0;i--){
-            int curri=str[i]-'0';
-            if(curri>pivot){
-                swap(str[i],str[ind]);
-                break;
-            }
-        }
-        sort(str.begin()+ind+1,str.end());
-        return str;
-    }
 public:
     string getPermutation(int n, int k) {
-        //  going with the with the brute force 
-        // will calculate the next_permutation till k times , 
-        //  create the string 
-        string str="";
+        vector<long long >fact(n+1,1);
+
+        // precompute the factoiral 
+        long long prod=1;
         for(int i=1;i<=n;i++){
-            str+=to_string(i);
+           fact[i]=prod;
+           prod=prod*(i+1);
         }
 
-        for(int i=1;i<k;i++){
-           str=next_permutation(str);
-        //    cout<<"index -> "<<i<<" and str is "<<str<<endl;
+        // intution is think via fixing one digit , how many permutations are possible 
+
+        k=k-1;
+        // indexed with 0 indexing 
+        vector<int>digits={1,2,3,4,5,6,7,8,9};
+        string res="";
+
+        for(int i=n;i>=1;i--){
+            int blocksize=fact[i-1];
+            int index=k/blocksize;
+            res+=to_string(digits[index]);
+            digits.erase(digits.begin()+index);
+            k=k%blocksize;
         }
 
-        return str;
+        return res;
+
     }
 };
