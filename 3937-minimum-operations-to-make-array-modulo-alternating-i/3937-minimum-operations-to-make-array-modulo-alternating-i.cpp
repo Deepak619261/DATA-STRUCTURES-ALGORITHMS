62d3b99d391b7379f4e690d1394c2b3c23744b7d
Deepak kumar <deepakarya140831@gmail.com>
2026-05-24 20:33:04 +0530

    Time: 3 ms (83.33%), Space: 24.9 MB (33.33%) - LeetHub

--- 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.cpp
+++ 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.cpp
@@ -3,25 +3,42 @@ public:
     int minOperations(vector<int>& nums, int k) {
         //  take i from zero to k 
         // put the conditions 
+        vector<int>evencost(k,0);
+        vector<int>oddcost(k,0);
+
+        // int temp=0;
+        for(int r=0;r<k;r++){
+            for(int i=0;i<nums.size();i++){
+                int diff=abs((nums[i]%k)-r);
+                diff=min(diff,k-diff);
+                if(i%2==0){
+                    evencost[r]+=diff;
+                }
+                else{
+                     oddcost[r]+=diff;
+                }
+            }
+
+        }
 
         int cnt=INT_MAX;
         for(int x=0;x<k;x++){
             for(int y=0;y<k;y++){
                 if(x==y)continue;
                 //  mentioned in problem statement 
-                int cost=0;
+               
 
-                for(int i=0;i<nums.size();i++){
-                    if(i%2==0){
-                        int diff=abs((nums[i]%k)-x);
-                        cost+=min(diff,k-diff);
-                    }
-                    else{
-                        int diff=abs((nums[i]%k)-y);
-                        cost+=min(diff,k-diff);
-                    }
-                }
-                cnt=min(cost,cnt);
+                // for(int i=0;i<nums.size();i++){
+                //     if(i%2==0){
+                //         int diff=abs((nums[i]%k)-x);
+                //         cost+=min(diff,k-diff);
+                //     }
+                //     else{
+                //         int diff=abs((nums[i]%k)-y);
+                //         cost+=min(diff,k-diff);
+                //     }
+                // }
+                cnt=min(cnt,evencost[x]+oddcost[y]);
                 // cnt+=something;
             }
         }
