4b88e68ec57796e8e5c0394200cfb8bee5e0d998
Deepak kumar <deepakarya140831@gmail.com>
2026-05-24 22:43:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (46.76%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int minOperations(string s) {
-
-        // this is the sibling of the question 3937
-    //    pairs will be -> (0,1) or (1,0)
-    int ans=INT_MAX;
-    //(0,1)
-        int cost=0;
-    for(int i=0;i<s.size();i++){
-        if(i%2==0){
-           cost+=s[i]-'0';
-        }
-        else{
-           cost+='1'-s[i];
-        }
-    }
-    ans=min(ans,cost);
     int n=s.size();
-    ans=min(ans,n-ans);
-    return ans;
-        
+        int costA = 0;
+for (int i = 0; i < n; i++) {
+    char expected = (i % 2 == 0) ? '0' : '1';
+    if (s[i] != expected) costA++;
+}
+    return min(costA,n-costA);
     }
 };
