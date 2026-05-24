266b9fc9b6df0758ff36ef7471e09d962284d32d
Deepak kumar <deepakarya140831@gmail.com>
2026-05-24 20:57:55 +0530

    Time: 4 ms (83.33%), Space: 25 MB (33.33%) - LeetHub

--- 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.cpp
+++ 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.cpp
@@ -21,30 +21,64 @@ public:
 
         }
 
-        int cnt=INT_MAX;
-        for(int x=0;x<k;x++){
-            for(int y=0;y<k;y++){
-                if(x==y)continue;
-                //  mentioned in problem statement 
-               
+        int e1=INT_MAX;
+        int ei1=-1;
+        int e2=INT_MAX;
 
-                // for(int i=0;i<nums.size();i++){
-                //     if(i%2==0){
-                //         int diff=abs((nums[i]%k)-x);
-                //         cost+=min(diff,k-diff);
-                //     }
-                //     else{
-                //         int diff=abs((nums[i]%k)-y);
-                //         cost+=min(diff,k-diff);
-                //     }
-                // }
-                cnt=min(cnt,evencost[x]+oddcost[y]);
-                // cnt+=something;
+        for(int r=0;r<k;r++){
+            if(evencost[r]<e1){
+                e2=e1;
+                e1=evencost[r];
+                ei1=r;
+            }
+            else if(evencost[r]<e2){
+                e2=evencost[r];
             }
         }
 
-        // time complexity -> O(n*k)+O(k^2)
 
-        return cnt;
+        int o1=INT_MAX;
+        int oi1=-1;
+        int o2=INT_MAX;
+
+        for(int r=0;r<k;r++){
+            if(oddcost[r]<o1){
+                o2=o1;
+                o1=oddcost[r];
+                oi1=r;
+            }
+            else if(oddcost[r]<o2){
+                o2=oddcost[r];
+            }
+        }
+
+        // int cnt=INT_MAX;
+        // for(int x=0;x<k;x++){
+        //     for(int y=0;y<k;y++){
+        //         if(x==y)continue;
+        //         //  mentioned in problem statement 
+               
+
+        //         // for(int i=0;i<nums.size();i++){
+        //         //     if(i%2==0){
+        //         //         int diff=abs((nums[i]%k)-x);
+        //         //         cost+=min(diff,k-diff);
+        //         //     }
+        //         //     else{
+        //         //         int diff=abs((nums[i]%k)-y);
+        //         //         cost+=min(diff,k-diff);
+        //         //     }
+        //         // }
+        //         cnt=min(cnt,evencost[x]+oddcost[y]);
+        //         // cnt+=something;
+        //     }
+        // }
+
+        // time complexity -> O(n*k)+O(k^2)
+        if(ei1!=oi1)return e1+o1;
+
+
+         return min(e1+o2,e2+o1);
+        // return cnt;
     }
 };
