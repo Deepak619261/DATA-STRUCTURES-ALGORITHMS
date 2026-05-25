cc3563bfa0529ffcfdc4ee386f5026e3c79dae03
Deepak kumar <deepakarya140831@gmail.com>
2026-05-26 00:22:05 +0530

    Time: 0 ms (100%), Space: 17.9 MB (6.13%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-         // maximum sum and maximum product subarray should lead you to the kadane's algo 
+        if(nums.size()==1)return nums[0];
+        int currMax=nums[0];
+        int currMin=nums[0];
+        int ans=currMax;
 
-         // i saw it from the solution but the here kadane will work from left and right both sides 
-         int maxi=INT_MIN;
-
-         int prod=1;
-
-         for(int i=0;i<nums.size();i++){
-            prod*=nums[i];
-            maxi=max(prod,maxi);
-            if(prod==0)prod=1;
-         }
-
-         prod=1;
-
-         for(int i=nums.size()-1;i>=0;i--){
-            prod*=nums[i];
-            maxi=max(prod,maxi);
-            if(prod==0)prod=1;
-         }
-
-
-         return maxi;
+        for(int i=1;i<nums.size();i++){
+            int maxi=max(nums[i],max(currMax*nums[i],currMin*nums[i]));
+            int mini=min(nums[i],min(currMax*nums[i],currMin*nums[i]));
+            currMax=maxi;
+            currMin=mini;
+            ans=max(ans,currMax);
+        }
+        return ans;
     }
 };
