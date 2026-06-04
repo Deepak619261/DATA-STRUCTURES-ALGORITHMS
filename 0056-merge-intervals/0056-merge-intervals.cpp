302724b37759a9977433ad1edc9de8d883c03c54
Deepak kumar <deepakarya140831@gmail.com>
2026-06-05 00:24:44 +0530

    Time: 4 ms (66.29%), Space: 24 MB (32.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,23 +3,28 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
-        int left=intervals[0][0];
-        int rightlim=intervals[0][1];
-
+        // return intervals;
+        int first=intervals[0][0];
+        int second=intervals[0][1];
         for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]>rightlim){
-                vector<int>temp={left,rightlim};
-                ans.push_back(temp);
-                left=intervals[i][0];
-                rightlim=intervals[i][1];
+            int x=intervals[i][0];
+            int y=intervals[i][1];
+            if((x<=second && y<=second)){
+                continue;
+            }
+            else if(x<=second){
+                second=y;
             }
             else{
-                rightlim=max(rightlim,intervals[i][1]);
+                ans.push_back({first,second});
+                first=x;
+                second=y;
             }
         }
-        
-        vector<int>temp={left,rightlim};
-        ans.push_back(temp);
+
+        vector<int>last={first,second};
+
+        if(ans.empty() || (!ans.empty() && ans.back()!=last))ans.push_back({first,second});
 
         return ans;
     }
