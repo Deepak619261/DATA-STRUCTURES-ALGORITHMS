08187122b35a0c43a1462913a08d125ebd7c402e
Deepak kumar <deepakarya140831@gmail.com>
2026-06-25 08:30:08 +0530

    Time: 77 ms (6.24%), Space: 38.5 MB (5.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,25 +1,45 @@
 class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        //  better appraoch 
-        unordered_map<string,vector<string>>mpp;
 
-        for(auto it:strs){
-            string srted=it;
-            sort(srted.begin(),srted.end());
-            if(mpp.contains(srted)){
-                mpp[srted].push_back(it);
-            }else{
-                mpp[srted].push_back(it);
-            }
+    string CovertToFrequencySignature(string str){
+        vector<int>freq(26,0);
+
+        for(auto it:str){
+            freq[it-'a']++;
         }
-
-        vector<vector<string>>ans;
-
-        for(auto it:mpp){
-            ans.push_back(it.second);
+        string ans="";
+        for(int i=0;i<26;i++){
+           ans+=(to_string(freq[i]))+'#';
         }
 
         return ans;
+
+        //  time complexity -> O(26*k)
+    }
+
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       // core idea is that frequency array of the same 
+       // so instead of sorting we can just keep the ferq array mapping , but in unordered_map , we can't keep something like vector<int> -> vector<string> 
+       // but we can keep someting like that in ordered_map , but time compelixty reaches to the log same as the sorting one so no benefit doing that 
+       // another thing to distinct the difference between array like 1,0,1 and 11,0,1 we use # inbetween to separate and we convert the fre array to string 
+
+       unordered_map<string,vector<string>>mpp;
+
+       vector<vector<string>>ans;
+       
+
+       for(auto it:strs){
+        //   cout<<" freq signature for "<<it<<" is"<<CovertToFrequencySignature(it)<<endl;
+          string signature=CovertToFrequencySignature(it);
+          mpp[signature].push_back(it);
+       }
+
+
+       for(auto it:mpp){
+          ans.push_back(it.second);
+       }
+
+       return ans;
+
     }
 };
