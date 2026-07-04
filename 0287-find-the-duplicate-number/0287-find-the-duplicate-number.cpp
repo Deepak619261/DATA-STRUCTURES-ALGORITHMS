e1fff968b08cdd0f7484b2aa099584fec716de22
Deepak kumar <deepakarya140831@gmail.com>
2026-07-04 17:12:20 +0530

    Time: 0 ms (100%), Space: 65.2 MB (41.54%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        //  we can use the floyd cycle detection algo here 
+        int fast=nums[0];
+        int slow=nums[0];
+
+
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(fast!=slow);
+
+        slow=nums[0];
+
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+
+        return fast;
+
+
+
+
+    }
+};
