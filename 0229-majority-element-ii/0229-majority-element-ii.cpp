11056adf3e2ec37a7383b5b405abea657643725a
Deepak kumar <deepakarya140831@gmail.com>
2026-07-10 00:52:56 +0530

    Time: 0 ms (100%), Space: 23.7 MB (64.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,55 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //  striaght forward appraoch that comes to mind is just use the map 
-        unordered_map<int,int>mpp;
+        int n=nums.size();
+        int cnt1=0;
+        int cnt2=0;
+
+        int ele1=-1;
+        int ele2=-1;
+
+
+        for(int i=0;i<nums.size();i++){
+            if(cnt1==0 && nums[i]!=ele2){
+                cnt1++;
+                ele1=nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=ele1 ){
+                cnt2++;
+                ele2=nums[i];
+            }
+            else if(ele1==nums[i]){
+                cnt1++;
+            }
+            else if(ele2==nums[i]){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1=0;
+        cnt2=0;
+
+        int limit=n/3;
 
         for(auto it:nums){
-            mpp[it]++;
+            if(it==ele1){
+                cnt1++;
+            }
+            else if(it==ele2){
+                cnt2++;
+            }
         }
 
-        vector<int>ans;
+        vector<int>res;
 
-        int lim=nums.size()/3;
+        if(cnt1>limit)res.push_back(ele1);
 
-        for(auto it:mpp){
-            if(it.second>lim)ans.push_back(it.first);
-        }
+        if(cnt2>limit)res.push_back(ele2);
 
-        return ans;
+        return res;
     }
 };
