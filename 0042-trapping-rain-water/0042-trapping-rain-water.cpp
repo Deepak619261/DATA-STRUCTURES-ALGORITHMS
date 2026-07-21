17ab6fde36c005160edbd8eaab9f43fd24f33abd
Deepak kumar <deepakarya140831@gmail.com>
2026-07-21 09:57:27 +0530

    Time: 4 ms (9.15%), Space: 27.2 MB (27.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        //  just maintain the two array representing the max of prefix and max suffix 
+
         int n=height.size();
 
-        //  two pointer apporaoch 
-        int left=0;
-        int right=n-1;
+        vector<int>prefix(n,0);
+        vector<int>suffix(n,0);
+        prefix[0]=height[0];
+        suffix[n-1]=height[n-1];
 
-        int leftmax=0;
-        int rightmax=0;
-        //  the whole idea resolves around the idea of water trapped as min(nums[left],nums[right])-nums[i]
-        int ans=0;
+        for(int i=1;i<height.size();i++){
+            prefix[i]=max(height[i],prefix[i-1]);
+        }
 
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=max(height[i],suffix[i+1]);
+        } 
 
-        while(left<=right){
-            if(height[left]<=height[right]){
-                //  means min will be on the left side which is needed to calcuate the answer
-                if(leftmax<=height[left]){
-                    leftmax=height[left];
-                }
-                else{
-                    ans+=leftmax-height[left];
-                }
-                left++;
-            }
-            else{
-                if(rightmax<=height[right]){
-                    rightmax=height[right];
-                }
-                else{
-                    ans+=rightmax-height[right];
-                }
-                right--;
+        // for(auto it:suffix){
+        //     cout<<it<<" ";
+        // }
+
+        int answer=0;
+
+        for(int i=1;i<n-1;i++){
+            int leftmax=prefix[i];
+            int rightmax=suffix[i];
+
+            // cout<<"for index "<<i<<" the leftmax is"<<leftmax<<" and rightmax is "<<rightmax<<endl;
+
+            if(height[i]<leftmax && height[i]<rightmax){
+                answer+=(min(leftmax,rightmax)-height[i]);
+            // cout<<" the answer we are adding is "<<(min(leftmax,rightmax)-height[i])<<endl;
             }
         }
 
-
-        return ans;
-
-        
+        return answer;
     }
 };
