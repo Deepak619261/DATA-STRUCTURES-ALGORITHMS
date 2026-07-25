986160bf3de0ac83b3f2be53a97990a8c2f9cf36
Deepak kumar <deepakarya140831@gmail.com>
2026-07-25 16:09:05 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,16 @@
 class Solution {
-    int solve(int i,int j,vector<vector<bool>>&visited,int m , int n,vector<vector<int>>&dp){
-        if(i>=m || j>=n || visited[i][j])return 0;
+    int solve(int i,int j,int m , int n,vector<vector<int>>&dp){
+        if(i>=m || j>=n)return 0;
 
 
         if(dp[i][j]!=-1)return dp[i][j];
 
         if(i==m-1 && j==n-1)return 1;
 
-        visited[i][j]=true;
-        int down=solve(i+1,j,visited,m,n,dp);
-        int right=solve(i,j+1,visited,m,n,dp);
-        visited[i][j]=false;
+        // visited[i][j]=true;
+        int down=solve(i+1,j,m,n,dp);
+        int right=solve(i,j+1,m,n,dp);
+        // visited[i][j]=false;
 
 
 
@@ -18,8 +18,22 @@ class Solution {
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<bool>>visited(m,vector<bool>(n,false));
+        // vector<vector<bool>>visited(m,vector<bool>(n,false));
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(0,0,visited,m,n,dp);
+        return solve(0,0,m,n,dp);
+        // dp[m-1][n-1]=1;
+
+        // for(int i=m-1;i>=0;i--){
+        //     for(int j=n-1;j>=0;j--){
+        //         int up=0;
+        //         int left=0;
+        //         if(i+1<m){
+        //           up=dp[i+1][j];
+        //         }
+        //         if(j+1<n){
+        //             left=dp[i][j+1];
+        //         }
+        //     }
+        // }
     }
 };
