c0114d14fd1d161621051c2b4097dc46a2785601
Deepak kumar <deepakarya140831@gmail.com>
2026-07-29 09:32:01 +0530

    Time: 0 ms (100%), Space: 14.8 MB (39.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,11 +5,22 @@ public:
 
         // vector<int>ans;
 
-        for(int i =0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
+        // for(int i =0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(nums[i]+nums[j]==target){
+        //             return {i,j};
+        //         }
+        //     }
+        // }
+
+        unordered_map<int,int>mpp;
+
+        for(int i=0;i<nums.size();i++){
+            if(mpp.contains(target-nums[i])){
+               return {i,mpp[target-nums[i]]};
+            }
+            else{
+                mpp[nums[i]]=i;
             }
         }
 
