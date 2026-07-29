3d89a6b352c39a7b6276aece40114e8a6d5b8cba
Deepak kumar <deepakarya140831@gmail.com>
2026-07-29 09:26:40 +0530

    Time: 43 ms (24.14%), Space: 14 MB (83.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i;j<nums.size();j++){
-                if(i==j){
-                    continue;
-                }
+        //the very brute force that comes to mind is O(n^2)
+
+        // vector<int>ans;
+
+        for(int i =0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
+                    return {i,j};
                 }
             }
         }
-    return ans;
+
+        return {};
     }
 };
