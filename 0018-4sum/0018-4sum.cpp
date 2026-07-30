c3f5e67068bce858330960a030dcad6b2ba35d6b
Deepak kumar <deepakarya140831@gmail.com>
2026-07-30 09:49:28 +0530

    Time: 1849 ms (5%), Space: 474.5 MB (5%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        //  lets go via the brute force first 
-        // set<vector<int>>st;
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
+        // vector<vector<int>>ans;
+
+        set<vector<int>>st;
 
         for(int i=0;i<nums.size();i++){
-            if(i!=0 && nums[i]==nums[i-1])continue;
             for(int j=i+1;j<nums.size();j++){
-                if(j>i+1 && nums[j]==nums[j-1])continue;
-                int s=j+1;
-                int e=nums.size()-1;
-                long long targetdash=(long long)target-(long long)nums[i]-(long long)nums[j];
+                unordered_set<long long>seen;
+                for(int k=j+1;k<nums.size();k++){
+                    long long required=(long long)target-nums[i]-nums[j]-nums[k];
 
-                while(s<e){
-                long long sum=(long long)nums[s]+(long long)nums[e];
-                
-
-                if(sum<targetdash){
-                    s++;
-                }
-                else if(sum>targetdash){
-                    e--;
-                }
-                else if(nums[s]+nums[e]==targetdash){
-                  ans.push_back({nums[i],nums[j],nums[s++],nums[e--]});
-                  while(s!=i+1 && s<e && nums[s]==nums[s-1]){
-                    s++;
-                }
-
-                while(e!=nums.size()-1 && s<e && nums[e]==nums[e+1]){
-                    e--;
-                }
-                }
+                    if(required >= INT_MIN && required <= INT_MAX &&seen.count((int)required)){
+                        vector<int>temp={nums[i],nums[j],nums[k],(int)required};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+                    seen.insert(nums[k]);
                 }
             }
         }
+        vector<vector<int>>ans(st.begin(),st.end());
 
-        // vector<vector<int>>ans(st.begin(),st.end());
         return ans;
+
     }
 };
