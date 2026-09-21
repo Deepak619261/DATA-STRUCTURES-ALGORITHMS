8c851774e1a1ae4636f2115176c693cd2fdd0653
Deepak kumar <deepakarya140831@gmail.com>
2026-09-21 21:44:40 +0530

    Time: 7 ms (4.47%), Space: 25.2 MB (5.27%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -1,28 +1,45 @@
 class Solution {
+    int gcd(int a,int b){
+        while(a>0 && b>0){
+            if(a>b){
+                a=a%b;
+            }
+            else{
+                b=b%a;
+            }
+        }
+        if(a==0)return b;
+        return a;
+    }
 public:
     bool hasGroupsSizeX(vector<int>& deck) {
-        unordered_map<int,int>mpp;
-        // for(auto it:deck){
-        //     mpp[it]++;
-        // }
+
+        vector<int>mpp(1e4,0);
 
         for(auto it:deck){
             mpp[it]++;
         }
+        
+        int g=-1;
 
-        for(int i=2;i<1e4;i++){
-            bool flag=true;
-            for(auto it:mpp){
-                if(it.second%i!=0){
-                   flag=false;
-                   continue;
+        for(int i=0;i<1e4;i++){
+            if(mpp[i]>0){
+                if(g==-1){
+                    g=mpp[i];
                 }
+                else{
+                    cout<<"before going "<<mpp[i]<<"annd  "<<g<<endl;
+                    g=gcd(g,mpp[i]);
+                    cout<<" after coming "<<g<<endl;
+                }
+                cout<<"at the occurence :- "<<mpp[i]<<" the g is :-"<<g<<endl;
             }
-            if(flag)return true;
         }
+        if(g>1)return true;
 
         return false;
 
 
+        
     }
 };
